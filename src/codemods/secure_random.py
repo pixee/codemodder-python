1494bcc7f5b1976e24090dd8401e19b1ebc2b1a2
clavedeluna <danalitovsky+git@gmail.com>
2023-04-28 08:49:29 -0700

    working import random testcase

--- src/codemods/secure_random.py
+++ src/codemods/secure_random.py
@@ -4,10 +4,20 @@ from libcst.codemod import CodemodContext, VisitorBasedCodemodCommand
 from .helper import split_module
 
 
+replacement_import = "import secrets"
+replacement_random_code = """secrets.SystemRandom()
+gen = secrets.SystemRandom()
+gen.uniform(0, 1)
+"""
+
+
 class SecureRandom(VisitorBasedCodemodCommand):
-    DESCRIPTION: str = "Replaces random.random with secrets."
+    DESCRIPTION: str = (
+        "Replaces random.{func} with more secure secrets library functions."
+    )
 
     def __init__(self, context: CodemodContext):
+        self.random_called = False
         super().__init__(context)
 
     def leave_Import(
@@ -27,30 +37,96 @@ class SecureRandom(VisitorBasedCodemodCommand):
             statements_after_imports,
         ) = split_module(original_node, updated_node)
 
+        if self.random_called:
+            first_line = cst.SimpleStatementLine(
+                body=[
+                    cst.Assign(
+                        targets=[
+                            cst.AssignTarget(
+                                target=cst.Name(
+                                    value="gen",
+                                ),
+                            ),
+                        ],
+                        value=cst.Call(
+                            func=cst.Attribute(
+                                value=cst.Name(
+                                    value="secrets",
+                                ),
+                                attr=cst.Name(
+                                    value="SystemRandom",
+                                ),
+                            ),
+                        ),
+                    )
+                ]
+            )
+
+            second_line = cst.SimpleStatementLine(
+                body=[
+                    cst.Expr(
+                        value=cst.Call(
+                            func=cst.Attribute(
+                                value=cst.Name(value="gen"),
+                                attr=cst.Name(value="uniform"),
+                            ),
+                            args=[
+                                cst.Arg(value=cst.Integer(value="0")),
+                                cst.Arg(
+                                    value=cst.Integer(value="1"),
+                                ),
+                            ],
+                        )
+                    ),
+                ]
+            )
+            new_code = [first_line, second_line]
+            statements_after_imports = new_code + statements_after_imports
+
         return updated_node.with_changes(
             body=[
                 *statements_before_imports,
                 *statements_until_add_imports,
                 *[
                     cst.parse_statement(
-                        "import secrets", config=updated_node.config_for_parsing
+                        replacement_import, config=updated_node.config_for_parsing
                     )
                 ],
+                cst.EmptyLine(
+                    indent=True,
+                    whitespace=cst.SimpleWhitespace(
+                        value="",
+                    ),
+                    comment=None,
+                    newline=cst.Newline(
+                        value=None,
+                    ),
+                ),
                 *statements_after_imports,
             ]
         )
 
-    def leave_Call(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
+    def leave_Expr(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
         if is_random_node(original_node):
-            self.modified_random = True
-            return updated_node.with_changes(func=cst.Name("secrets"))
+            self.random_called = True
+            return cst.RemoveFromParent()
         return updated_node
 
+    # def leave_Call(self, original_node: cst.Call, updated_node: cst.Call) -> cst.Call:
+    #     if is_random_node(original_node):
+    #         breakpoint()
+    #         return updated_node.with_changes(func=cst.Name("secrets.SystemRandom()"))
+    #     return updated_node
 
-def is_random_node(node: cst.Call) -> bool:
+
+def is_random_node(node: cst.Expr) -> bool:
     return matchers.matches(
         node,
-        matchers.Call(func=matchers.Attribute(value=matchers.Name(value="random"))),
+        matchers.Expr(
+            value=matchers.Call(
+                func=matchers.Attribute(value=matchers.Name(value="random"))
+            )
+        ),
     )
 
 
--- tests/codemods/test_secure_random.py
+++ tests/codemods/test_secure_random.py
@@ -5,6 +5,13 @@ from src.codemods.secure_random import SecureRandom
 
 
 class TestSecureRandom:
+    def run_and_assert(self, input_code, expexted_output):
+        input_tree = cst.parse_module(input_code)
+        command_instance = SecureRandom(CodemodContext())
+        output_tree = command_instance.transform_module(input_tree)
+
+        assert output_tree.code == expexted_output
+
     @pytest.mark.parametrize(
         "input_code,expexted_output",
         [
@@ -16,7 +23,8 @@ var = "hello"
         """,
                 """import secrets
 
-secrets.randbits(10)
+gen = secrets.SystemRandom()
+gen.uniform(0, 1)
 var = "hello"
         """,
             ),
@@ -28,50 +36,74 @@ var = "hello"
             """,
                 """import secrets
 
-secrets.randbits(10)
+gen = secrets.SystemRandom()
+gen.uniform(0, 1)
 var = "hello"
-            """,
-            ),
-            # other functions in random are not changed
-            (
-                f"""import random
-
-    random.uniform(1, 2)
-            """,
-                """import random
-
-    random.uniform(1, 2)
-            """,
-            ),
-            # random import isn't removed if other functions are used
-            (
-                f"""import random
-
-    random.random()
-    random.uniform(1, 2)
-            """,
-                """import random
-import secrets
-secrets.randbits(10)
-random.uniform(1, 2)
-            """,
-            ),
-            # code leads to error
-            (
-                f"""random.random()
-
-    import random
-            """,
-                """random.random()
-
-    import random
             """,
             ),
         ],
     )
-    def test_expected_mod(self, input_code, expexted_output):
-        input_tree = cst.parse_module(input_code)
-        command_instance = SecureRandom(CodemodContext())
-        output_tree = command_instance.transform_module(input_tree)
+    def test_random_random(self, input_code, expexted_output):
+        self.run_and_assert(input_code, expexted_output)
 
-        assert output_tree.code == expexted_output
+    @pytest.mark.parametrize(
+        "input_code,expexted_output",
+        [
+            (
+                f"""import random
+
+random.randint(0, 10)
+var = "hello"
+        """,
+                """import secrets
+
+    gen = secrets.SystemRandom()
+    gen.randint(0, 10)
+    var = "hello"
+            """,
+            ),
+            (
+                f"""from random import randint
+
+randint(0, 10)
+var = "hello"
+            """,
+                """import secrets
+
+    gen = secrets.SystemRandom()
+    gen.randint(0, 10)
+    var = "hello"
+                """,
+            ),
+        ],
+    )
+    def test_random_randint(self, input_code, expexted_output):
+        self.run_and_assert(input_code, expexted_output)
+
+    def test_random_nameerror(self):
+        input_code = f"""random.random()
+
+import random
+            """
+        expexted_output = input_code
+        self.run_and_assert(input_code, expexted_output)
+
+    def test_random_multifunctions(self):
+        # Test that `random` import isn't removed if code uses part of the random
+        # library that isn't part of our codemods. If we add the function as one of
+        # our codemods, this test would change.
+        input_code = f"""import random
+
+random.random()
+random.randrange(10)
+        """
+
+        expexted_output = """import random
+import secrets
+
+gen = secrets.SystemRandom()
+gen.uniform(0, 1)
+random.randrange(10)
+    """
+
+        self.run_and_assert(input_code, expexted_output)
