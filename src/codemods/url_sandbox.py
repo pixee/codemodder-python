8c0e02faad9ce33f7363bdaed0c381a9a4b1cc56
clavedeluna <danalitovsky+git@gmail.com>
2023-05-09 09:42:03 -0700

    add url sandbox for requests.get

---
+++ src/codemods/url_sandbox.py
@@ -0,0 +1,92 @@
+import libcst as cst
+import libcst.matchers as matchers
+from libcst.codemod import CodemodContext, VisitorBasedCodemodCommand
+
+
+replacement_import = "safe_requests"
+
+
+class UrlSandbox(VisitorBasedCodemodCommand):
+    DESCRIPTION: str = (
+        "Replaces request.{func} with more secure safe_request library functions."
+    )
+
+    def __init__(self, context: CodemodContext):
+        super().__init__(context)
+
+    def leave_Import(
+        self, original_node: cst.Import, updated_node: cst.Import
+    ) -> cst.Import:
+        if is_requests_import(original_node):
+            return updated_node.with_changes(
+                names=[
+                    updated_node.names[0].with_changes(
+                        name=updated_node.names[0].name.with_changes(
+                            value=replacement_import
+                        )
+                    )
+                ]
+            )
+
+        return updated_node
+
+    def leave_ImportFrom(
+        self, original_node: cst.ImportFrom, updated_node: cst.ImportFrom
+    ) -> cst.ImportFrom:
+
+        if is_requests_import(original_node):
+            return updated_node.with_changes(
+                module=updated_node.module.with_changes(value=replacement_import)
+            )
+
+        return updated_node
+
+    def leave_Expr(self, original_node: cst.Expr, updated_node: cst.Expr) -> cst.Expr:
+        if is_requests_get_node(original_node):
+            updated_import_call = updated_node.value.func.value.with_changes(
+                value=replacement_import
+            )
+
+            return updated_node.with_changes(
+                value=updated_node.value.with_changes(
+                    func=updated_node.value.func.with_changes(value=updated_import_call)
+                )
+            )
+
+        return updated_node
+
+
+def is_requests_get_node(node: cst.Expr) -> bool:
+    """
+    Check to see if either: requests.get() or get() is called.
+
+    :param node:
+    :return: bool
+    """
+    library_dot_get = matchers.Expr(
+        value=matchers.Call(
+            func=matchers.Attribute(
+                value=matchers.Name(value="requests"), attr=matchers.Name(value="get")
+            )
+        )
+    )
+
+    return matchers.matches(
+        node,
+        library_dot_get,
+    )
+
+
+def is_requests_import(node: cst.Import | cst.ImportFrom) -> bool:
+    import_alias_requests = matchers.ImportAlias(name=matchers.Name(value="requests"))
+    import_alias_get = matchers.ImportAlias(name=matchers.Name(value="get"))
+
+    direct_import = matchers.Import(names=[import_alias_requests])
+    from_requests_import_get = matchers.ImportFrom(
+        module=cst.Name(value="requests"), names=[import_alias_get]
+    )
+
+    return matchers.matches(
+        node,
+        matchers.OneOf(direct_import, from_requests_import_get),
+    )
---
+++ tests/codemods/test_url_sandbox.py
@@ -0,0 +1,83 @@
+import libcst as cst
+from libcst.codemod import CodemodContext
+import pytest
+from src.codemods.url_sandbox import UrlSandbox
+
+
+class TestUrlSandbox:
+    def run_and_assert(self, input_code, expexted_output):
+        input_tree = cst.parse_module(input_code)
+        command_instance = UrlSandbox(CodemodContext())
+        output_tree = command_instance.transform_module(input_tree)
+
+        assert output_tree.code == expexted_output
+
+    @pytest.mark.parametrize(
+        "input_code,expexted_output",
+        [
+            (
+                f"""import requests
+
+requests.get("www.google.com")
+var = "hello"
+        """,
+                """import safe_requests
+
+safe_requests.get("www.google.com")
+var = "hello"
+        """,
+            ),
+            (
+                f"""from requests import get
+
+get("www.google.com")
+var = "hello"
+            """,
+                """from safe_requests import get
+
+get("www.google.com")
+var = "hello"
+            """,
+            ),
+        ],
+    )
+    def test_requests(self, input_code, expexted_output):
+        self.run_and_assert(input_code, expexted_output)
+
+    @pytest.mark.skip()
+    def test_requests_nameerror(self):
+        input_code = f"""requests.get("www.google.com")
+
+import requests
+            """
+        expexted_output = input_code
+        self.run_and_assert(input_code, expexted_output)
+
+    @pytest.mark.skip()
+    def test_requests_multifunctions(self):
+        # Test that `requests` import isn't removed if code uses part of the requests
+        # library that isn't part of our codemods. If we add the function as one of
+        # our codemods, this test would change.
+        input_code = f"""import requests
+
+requests.get("www.google.com")
+requests.status_codes.codes.FORBIDDEN
+        """
+
+        expexted_output = """import requests
+import safe_requests
+
+safe_requests.get("www.google.com")
+requests.status_codes.codes.FORBIDDEN
+    """
+
+        self.run_and_assert(input_code, expexted_output)
+
+    @pytest.mark.skip()
+    def test_custom_get(self):
+        input_code = f"""from app_funcs import get
+
+get("www.google.com")
+    """
+        expexted_output = input_code
+        self.run_and_assert(input_code, expexted_output)
