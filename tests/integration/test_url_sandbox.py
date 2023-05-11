cbd005bf73cc0a0d7495bd01d516b35516271e7e
clavedeluna <danalitovsky+git@gmail.com>
2023-05-11 12:10:28 -0700

    add integration tests

--- tests/integration/test_url_sandbox.py
+++ tests/integration/test_url_sandbox.py
@@ -2,13 +2,13 @@ import mock
 from codemodder.__main__ import parse_args, run
 
 
-class TestSecureRandom:
+class TestUrlSandbox:
     @mock.patch("builtins.print")
     def test_result(self, mock_print):
         argv = parse_args(
-            ["tests/samples/", "--output", "here.txt", "--codemod=secure-random"]
+            ["tests/samples/", "--output", "here.txt", "--codemod=url-sandbox"]
         )
         run(argv)
         calls = mock_print.call_args_list
-        assert "import secrets" in calls[3][0][0]
-        assert "import requests" in calls[5][0][0]
+        assert "import random" in calls[3][0][0]
+        assert "import safe_requests" in calls[7][0][0]
