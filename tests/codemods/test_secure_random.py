1139c1189ca7745fadb888089d5e09cbc67566aa
clavedeluna <danalitovsky+git@gmail.com>
2023-06-20 07:55:20 -0300

    fix secure random unit tests

--- tests/codemods/test_secure_random.py
+++ tests/codemods/test_secure_random.py
@@ -5,47 +5,69 @@ from codemodder.codemods.secure_random import SecureRandom
 
 
 class TestSecureRandom:
+    RESULTS_BY_ID = {
+        "secure-random": [
+            {
+                "fingerprints": {"matchBasedId/v1": "3f3a "},
+                "locations": [
+                    {
+                        "physicalLocation": {
+                            "artifactLocation": {
+                                "uri": "tests/samples/insecure_random.py ",
+                                "uriBaseId": "%SRCROOT% ",
+                            },
+                            "region": {
+                                "endColumn": 16,
+                                "endLine": 3,
+                                "snippet": {"text": "random.random() "},
+                                "startColumn": 1,
+                                "startLine": 3,
+                            },
+                        }
+                    }
+                ],
+                "message": {"text": "Insecure Random "},
+                "properties": {},
+                "ruleId": "codemodder.codemods.semgrep.secure-random ",
+            }
+        ]
+    }
+
     def run_and_assert(self, input_code, expexted_output):
-        # TODO tests will have to be changed since we need the semgrep results
         input_tree = cst.parse_module(input_code)
-        command_instance = SecureRandom(CodemodContext(), {})
+        command_instance = SecureRandom(CodemodContext(), self.RESULTS_BY_ID)
         output_tree = command_instance.transform_module(input_tree)
 
         assert output_tree.code == expexted_output
 
-    @pytest.mark.parametrize(
-        "input_code,expexted_output",
-        [
-            (
-                """import random
+    def test_import_random(self):
+        input_code = """import random
 
 random.random()
 var = "hello"
-""",
-                """import secrets
-
+"""
+        expexted_output = """import secrets
 gen = secrets.SystemRandom()
+
 gen.uniform(0, 1)
 var = "hello"
-""",
-            ),
-            (
-                """from random import random
+"""
+
+        self.run_and_assert(input_code, expexted_output)
+
+    @pytest.mark.skip()
+    def test_from_random(self):
+        input_code = """from random import random
 
 random()
 var = "hello"
-""",
-                """import secrets
+"""
+        expexted_output = """import secrets
 
 gen = secrets.SystemRandom()
 gen.uniform(0, 1)
 var = "hello"
-""",
-            ),
-        ],
-    )
-    @pytest.mark.skip()
-    def test_random_random(self, input_code, expexted_output):
+"""
         self.run_and_assert(input_code, expexted_output)
 
     @pytest.mark.parametrize(
@@ -93,10 +115,9 @@ random.random()
 csv.excel
 """,
                 """import secrets
-
 gen = secrets.SystemRandom()
-gen.uniform(0, 1)
 import csv
+gen.uniform(0, 1)
 csv.excel
 """,
             ),
@@ -107,29 +128,24 @@ random.random()
 excel
 """,
                 """import secrets
-
 gen = secrets.SystemRandom()
-gen.uniform(0, 1)
 from csv import excel
+gen.uniform(0, 1)
 excel
 """,
             ),
         ],
     )
-    @pytest.mark.skip()
     def test_random_other_import_untouched(self, input_code, expexted_output):
         self.run_and_assert(input_code, expexted_output)
 
-    @pytest.mark.skip()
     def test_random_nameerror(self):
         input_code = """random.random()
 
-import random
-            """
+import random"""
         expexted_output = input_code
         self.run_and_assert(input_code, expexted_output)
 
-    @pytest.mark.skip()
     def test_random_multifunctions(self):
         # Test that `random` import isn't removed if code uses part of the random
         # library that isn't part of our codemods. If we add the function as one of
@@ -138,12 +154,12 @@ import random
 
 random.random()
 random.__all__
-        """
+"""
 
-        expexted_output = """import random
-import secrets
-
+        expexted_output = """import secrets
 gen = secrets.SystemRandom()
+import random
+
 gen.uniform(0, 1)
 random.__all__
 """
