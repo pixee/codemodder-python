a3af32d7f2d5ecbc8d552b99ee73f031b4354583
clavedeluna <danalitovsky+git@gmail.com>
2023-06-22 16:09:54 -0300

    test codetf reporter

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,25 @@ import mock
 
 
 @pytest.fixture(autouse=True, scope="module")
-def disable_file_writing():
+def disable_write_report():
     """
     Unit tests should not write analysis report or update any source files.
     """
     patch_write_report = mock.patch(
         "codemodder.report.codetf_reporter.CodeTF.write_report"
     )
-    patch_update_code = mock.patch("codemodder.__main__.update_code")
+
     patch_write_report.start()
-    patch_update_code.start()
     yield
     patch_write_report.stop()
+
+
+@pytest.fixture(autouse=True, scope="module")
+def disable_update_code():
+    """
+    Unit tests should not write analysis report or update any source files.
+    """
+    patch_update_code = mock.patch("codemodder.__main__.update_code")
+    patch_update_code.start()
+    yield
     patch_update_code.stop()
---
+++ tests/report/test_codetf_reporter.py
@@ -0,0 +1,29 @@
+import json
+import mock
+import pytest
+from codemodder.report.codetf_reporter import CodeTF
+
+
+@pytest.fixture(autouse=True, scope="module")
+def disable_write_report():
+    """Disable write_report mocking so it can be properlly tested"""
+    patch_open = mock.patch("builtins.open")
+    patch_open.start()
+    yield
+    patch_open.stop()
+
+
+class TestCodeTfReporter:
+    @mock.patch("json.dump")
+    def test_write_report(self, mock_dump):
+        reporter = CodeTF()
+        res = reporter.write_report("any/file")
+        assert res == 0
+        mock_dump.assert_called()
+
+    @mock.patch("json.dump", side_effect=json.JSONDecodeError)
+    def test_write_report_fails(self, mock_dump):
+        reporter = CodeTF()
+        res = reporter.write_report("any/file")
+        assert res == 2
+        mock_dump.assert_called()
