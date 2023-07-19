d023487b4749b3087601cc3f6ffbb2315059838c
clavedeluna <danalitovsky+git@gmail.com>
2023-07-19 07:32:15 -0300

    inherit from DM

--- codemodder/dependency_manager.py
+++ codemodder/dependency_manager.py
@@ -1,8 +1,7 @@
 from dependency_manager import DependencyManagerAbstract
+from codemodder import global_state
 
 
 class DependencyManager(DependencyManagerAbstract):
     def get_parent_dir(self):
-        """"""
-        return "tests/samples"
-        # self.context.directory
+        return global_state.DIRECTORY
