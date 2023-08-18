de054b14fdd50bae5f4d621df373e3607af938da
clavedeluna <danalitovsky+git@gmail.com>
2023-08-18 08:23:32 -0300

    create mixin for visitor

--- codemodder/codemods/base_visitor.py
+++ codemodder/codemods/base_visitor.py
@@ -3,32 +3,7 @@ from libcst.codemod import ContextAwareVisitor, VisitorBasedCodemodCommand
 from libcst.metadata import PositionProvider
 
 
-class BaseTransformer(VisitorBasedCodemodCommand):
-    METADATA_DEPENDENCIES = (PositionProvider,)
-
-    def __init__(self, context, results, line_exclude=None, line_include=None):
-        super().__init__(context)
-        self.results = results
-        self.line_exclude = line_exclude or []
-        self.line_include = line_include or []
-
-    def filter_by_result(self, pos_to_match):
-        all_pos = [extract_pos_from_result(result) for result in self.results]
-        return any(match_pos(pos_to_match, position) for position in all_pos)
-
-    def filter_by_path_includes_or_excludes(self, pos_to_match):
-        """
-        Returns True if the node, whose position in the file is pos_to_match, matches any of the lines specified in the path-includes or path-excludes flags.
-        """
-        # excludes takes precedence if defined
-        if self.line_exclude:
-            return not any(match_line(pos_to_match, line) for line in self.line_exclude)
-        if self.line_include:
-            return any(match_line(pos_to_match, line) for line in self.line_include)
-        return True
-
-
-class BaseVisitor(ContextAwareVisitor):
+class UtilsMixin:
     METADATA_DEPENDENCIES: Tuple[Any, ...] = (PositionProvider,)
 
     def __init__(self, context, results, line_exclude=None, line_include=None):
@@ -51,6 +26,21 @@ class BaseVisitor(ContextAwareVisitor):
         if self.line_include:
             return any(match_line(pos_to_match, line) for line in self.line_include)
         return True
+
+    def node_position(self, node):
+        # See https://github.com/Instagram/LibCST/blob/main/libcst/_metadata_dependent.py#L112
+        return self.get_metadata(self.METADATA_DEPENDENCIES[0], node)
+
+    def lineno_for_node(self, node):
+        return self.node_position(node).start.line
+
+
+class BaseTransformer(UtilsMixin, VisitorBasedCodemodCommand):
+    ...
+
+
+class BaseVisitor(UtilsMixin, ContextAwareVisitor):
+    ...
 
 
 def match_line(pos, line):
