8fa9e087a307b6c67c8fe1ed0d31280ffbbb7903
clavedeluna <danalitovsky+git@gmail.com>
2023-08-29 08:18:33 -0300

    create api.base_codemod & transtion fstr to api

--- codemodder/codemods/remove_unnecessary_f_str.py
+++ codemodder/codemods/remove_unnecessary_f_str.py
@@ -1,26 +1,18 @@
 import libcst as cst
-from libcst.codemod import CodemodContext
-from libcst.codemod.commands.unnecessary_format_string import UnnecessaryFormatString
-from libcst.metadata import PositionProvider
-from codemodder.codemods.change import Change
-import libcst.matchers as m
-from codemodder.codemods.base_codemod import (
-    BaseCodemod,
-    CodemodMetadata,
-    ReviewGuidance,
+from libcst.codemod import (
+    CodemodContext,
 )
+from libcst.codemod.commands.unnecessary_format_string import UnnecessaryFormatString
+import libcst.matchers as m
+from codemodder.codemods.base_codemod import ReviewGuidance
+from codemodder.codemods.api import BaseCodemod
 from codemodder.file_context import FileContext
 
 
 class RemoveUnnecessaryFStr(BaseCodemod, UnnecessaryFormatString):
-    METADATA = CodemodMetadata(
-        DESCRIPTION=UnnecessaryFormatString.DESCRIPTION,
-        NAME="remove-unnecessary-f-str",
-        REVIEW_GUIDANCE=ReviewGuidance.MERGE_WITHOUT_REVIEW,
-    )
-    CHANGE_DESCRIPTION = UnnecessaryFormatString.DESCRIPTION
-
-    METADATA_DEPENDENCIES = (PositionProvider,)
+    NAME = "remove-unnecessary-f-str"
+    REVIEW_GUIDANCE = ReviewGuidance.MERGE_WITHOUT_REVIEW
+    DESCRIPTION = UnnecessaryFormatString.DESCRIPTION
 
     def __init__(self, codemod_context: CodemodContext, file_context: FileContext):
         UnnecessaryFormatString.__init__(self, codemod_context)
@@ -34,8 +26,5 @@ class RemoveUnnecessaryFStr(BaseCodemod, UnnecessaryFormatString):
     ):
         transformed_node = super()._check_formatted_string(_original_node, updated_node)
         if not _original_node.deep_equals(transformed_node):
-            line_number = self.lineno_for_node(_original_node)
-            self.CHANGES_IN_FILE.append(
-                Change(str(line_number), self.CHANGE_DESCRIPTION).to_json()
-            )
+            self.report_change(_original_node)
         return transformed_node
