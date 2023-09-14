5b69d29ac84f34a74c28401d96c9d91311f49b2b
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-09-14 08:16:01 -0300

    HTTPSConnection sample change

--- tests/samples/http_connection.py
+++ tests/samples/http_connection.py
@@ -1,4 +1,8 @@
 import urllib3
+from urllib3 import HTTPConnectionPool as something
+import urllib3.connectionpool as pool
 
 urllib3.HTTPConnectionPool("localhost", "80")
 urllib3.connectionpool.HTTPConnectionPool("localhost", "80")
+something("localhost", "80")
+pool.HTTPConnectionPool("localhost", "80")
