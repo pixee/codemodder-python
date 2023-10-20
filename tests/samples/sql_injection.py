975b37aa1e851c3a9f11687fc18e4149af3816bf
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-10-20 10:03:21 -0300

    Fixed SQLQueryParameterization integration test

--- tests/samples/sql_injection.py
+++ tests/samples/sql_injection.py
@@ -1,11 +1,14 @@
 import sqlite3
 
-connection = sqlite3.connect("my_db.db")
+connection = sqlite3.connect("tests/samples/my_db.db")
 
-def foo(cursor: sqlite3.Cursor, name: str, phone:str):
+
+def foo(cursor: sqlite3.Cursor, name: str, phone: str):
     a = "SELECT * FROM Users"
-    b = "WHERE name ='" + name
+    b = " WHERE name ='" + name
     c = "' AND phone = '" + phone + "'"
-    cursor.execute(a + b + c)
+    r = cursor.execute(a + b + c)
+    print(r.fetchone())
 
-foo(connection.cursor(), 'Jenny', '867-5309')
+
+foo(connection.cursor(), "Jenny", "867-5309")
