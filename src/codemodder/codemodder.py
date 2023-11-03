f20e61f64b5a8e8911613f7b1bd0ea4a05adf4fa
Daniel D'Avella <dan.davella@pixee.ai>
2023-11-03 15:22:12 -0400

    Work around bug in difflib

--- src/codemodder/codemodder.py
+++ src/codemodder/codemodder.py
@@ -48,6 +48,21 @@ def find_semgrep_results(
     return {rule_id for file_changes in results.values() for rule_id in file_changes}
 
 
+def create_diff(original_tree: cst.Module, new_tree: cst.Module) -> str:
+    diff_lines = list(
+        difflib.unified_diff(
+            original_tree.code.splitlines(keepends=True),
+            new_tree.code.splitlines(keepends=True),
+        )
+    )
+    # All but the last diff line should end with a newline
+    # The last diff line should be preserved as-is (with or without a newline)
+    diff_lines = [
+        line if line.endswith("\n") else line + "\n" for line in diff_lines[:-1]
+    ] + [diff_lines[-1]]
+    return "".join(diff_lines)
+
+
 def apply_codemod_to_file(
     base_directory: Path,
     file_context,
@@ -68,12 +83,7 @@ def apply_codemod_to_file(
     if output_tree.deep_equals(source_tree):
         return False
 
-    diff = "".join(
-        difflib.unified_diff(
-            source_tree.code.splitlines(1), output_tree.code.splitlines(1)
-        )
-    )
-
+    diff = create_diff(source_tree, output_tree)
     change_set = ChangeSet(
         str(file_context.file_path.relative_to(base_directory)),
         diff,
