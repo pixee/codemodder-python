14e501316cc750470b99f1a3a437d4e7d087fe06
Daniel D'Avella <dan.davella@pixee.ai>
2023-11-20 13:07:29 -0500

    Fix indentation for enable-jinja2-autoescape tests

--- tests/codemods/test_enable_jinja2_autoescape.py
+++ tests/codemods/test_enable_jinja2_autoescape.py
@@ -9,79 +9,92 @@ class TestEnableJinja2Autoescape(BaseSemgrepCodemodTest):
         assert self.codemod.name() == "enable-jinja2-autoescape"
 
     def test_import(self, tmpdir):
-        input_code = """import jinja2
-env = jinja2.Environment()
-var = "hello"
-"""
-        expexted_output = """import jinja2
-env = jinja2.Environment(autoescape=True)
-var = "hello"
-"""
+        input_code = """
+        import jinja2
+        env = jinja2.Environment()
+        var = "hello"
+        """
+        expexted_output = """
+        import jinja2
+        env = jinja2.Environment(autoescape=True)
+        var = "hello"
+        """
 
         self.run_and_assert(tmpdir, input_code, expexted_output)
 
     def test_import_with_arg(self, tmpdir):
-        input_code = """import jinja2
-env = jinja2.Environment(autoescape=False)
-var = "hello"
-"""
-        expexted_output = """import jinja2
-env = jinja2.Environment(autoescape=True)
-var = "hello"
-"""
+        input_code = """
+        import jinja2
+        env = jinja2.Environment(autoescape=False)
+        var = "hello"
+        """
+        expexted_output = """
+        import jinja2
+        env = jinja2.Environment(autoescape=True)
+        var = "hello"
+        """
 
         self.run_and_assert(tmpdir, input_code, expexted_output)
 
     def test_import_with_more_args(self, tmpdir):
-        input_code = """import jinja2
-env = jinja2.Environment(loader=some_loader, autoescape=False)
-var = "hello"
-"""
-        expexted_output = """import jinja2
-env = jinja2.Environment(loader=some_loader, autoescape=True)
-var = "hello"
-"""
+        input_code = """
+        import jinja2
+        env = jinja2.Environment(loader=some_loader, autoescape=False)
+        var = "hello"
+        """
+        expexted_output = """
+        import jinja2
+        env = jinja2.Environment(loader=some_loader, autoescape=True)
+        var = "hello"
+        """
 
         self.run_and_assert(tmpdir, input_code, expexted_output)
 
     def test_import_with_other_args(self, tmpdir):
-        input_code = """import jinja2
-env = jinja2.Environment(loader=some_loader)
-var = "hello"
-"""
-        expexted_output = """import jinja2
-env = jinja2.Environment(loader=some_loader, autoescape=True)
-var = "hello"
-"""
+        input_code = """
+        import jinja2
+        env = jinja2.Environment(loader=some_loader)
+        var = "hello"
+        """
+        expexted_output = """
+        import jinja2
+        env = jinja2.Environment(loader=some_loader, autoescape=True)
+        var = "hello"
+        """
 
         self.run_and_assert(tmpdir, input_code, expexted_output)
 
     def test_from_import(self, tmpdir):
-        input_code = """from jinja2 import Environment
-env = Environment()
-var = "hello"
-"""
-        expexted_output = """from jinja2 import Environment
-env = Environment(autoescape=True)
-var = "hello"
-"""
+        input_code = """
+        from jinja2 import Environment
+        env = Environment()
+        var = "hello"
+        """
+        expexted_output = """
+        from jinja2 import Environment
+        env = Environment(autoescape=True)
+        var = "hello"
+        """
         self.run_and_assert(tmpdir, input_code, expexted_output)
 
     def test_import_alias(self, tmpdir):
-        input_code = """import jinja2 as templating
-env = templating.Environment()
-var = "hello"
-"""
-        expexted_output = """import jinja2 as templating
-env = templating.Environment(autoescape=True)
-var = "hello"
-"""
+        input_code = """
+        import jinja2 as templating
+        env = templating.Environment()
+        var = "hello"
+        """
+        expexted_output = """
+        import jinja2 as templating
+        env = templating.Environment(autoescape=True)
+        var = "hello"
+        """
         self.run_and_assert(tmpdir, input_code, expexted_output)
 
     def test_autoescape_enabled(self, tmpdir):
-        input_code = """import jinja2
-env = jinja2.Environment(autoescape=True)
-var = "hello"
-"""
+        input_code = """
+        import jinja2
+        env = jinja2.Environment(autoescape=True)
+        var = "hello"
+        """
         expexted_output = input_code
         self.run_and_assert(tmpdir, input_code, expexted_output)
