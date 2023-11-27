ee7c8f3fe3d5a6c717f1d3ce62f39ad8e51c362d
clavedeluna <danalitovsky+git@gmail.com>
2023-11-27 18:31:57 -0300

    compute new pyproject line nums

--- src/codemodder/dependency_management/pyproject_writer.py
+++ src/codemodder/dependency_management/pyproject_writer.py
@@ -21,14 +21,17 @@ class PyprojectWriter(DependencyWriter):
             tomlkit.dumps(original).split("\n"), tomlkit.dumps(pyproject).split("\n")
         )
 
+        added_lines = self.calc_new_line_nums(original, pyproject)
+
         if not dry_run:
             with open(self.path, "w") as f:
                 tomlkit.dump(pyproject, f)
 
         # TODO: compute the correct line num, maybe use the diff?
+        last_dep_line = ""
         changes = [
             Change(
-                lineNumber=len(original) + i + 1,
+                lineNumber=added_lines[i],
                 description=dep.build_description(),
                 # Contextual comments should be added to the right side of split diffs
                 properties={
@@ -50,3 +53,36 @@ class PyprojectWriter(DependencyWriter):
     def _parse_file(self):
         with open(self.path) as f:
             return tomlkit.load(f)
+
+    def calc_new_line_nums(self, original, updated):
+        added_lines = []
+        current_line_number = 0
+        import difflib
+
+        diff_lines = list(
+            difflib.unified_diff(
+                tomlkit.dumps(original).split("\n"),
+                tomlkit.dumps(updated).split("\n"),
+                lineterm="",
+            )
+        )
+        for line in diff_lines:
+            if line.startswith("@@"):
+                # Extract the starting line number for the updated file from the diff metadata.
+                # The format is @@ -x,y +a,b @@, where a is the starting line number in the updated file.
+                start_line = line.split(" ")[2]
+                current_line_number = (
+                    int(start_line.split(",")[0][1:]) - 1
+                )  # Subtract 1 because line numbers are 1-indexed
+
+            elif line.startswith("+"):
+                # Increment line number for each line in the updated file
+                current_line_number += 1
+                if not line.startswith("++"):  # Ignore the diff metadata lines
+                    added_lines.append(current_line_number)
+
+            elif not line.startswith("-"):
+                # Increment line number for unchanged/context lines
+                current_line_number += 1
+
+        return added_lines
