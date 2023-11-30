54d1f10a3def7135c131d5e18300cb8c0510e65e
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-11-30 07:41:15 -0300

    Added encoding detection to requirements parser

--- src/codemodder/project_analysis/file_parsers/requirements_txt_file_parser.py
+++ src/codemodder/project_analysis/file_parsers/requirements_txt_file_parser.py
@@ -1,6 +1,9 @@
+from typing import Optional
 from codemodder.project_analysis.file_parsers.package_store import PackageStore
 from pathlib import Path
 from .base_parser import BaseParser
+import chardet
+from codemodder.logging import logger
 
 
 class RequirementsTxtParser(BaseParser):
@@ -8,16 +11,27 @@ class RequirementsTxtParser(BaseParser):
     def file_name(self):
         return "requirements.txt"
 
-    def _parse_file(self, file: Path):
-        with open(file, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-
-        return PackageStore(
-            type=self.file_name,
-            file=str(file),
-            dependencies=set(self._parse_dependencies(lines)),
-            # requirements.txt files do not declare py versions explicitly
-            # though we could create a heuristic by analyzing each dependency
-            # and extracting py versions from them.
-            py_versions=[],
-        )
+    def _parse_file(self, file: Path) -> Optional[PackageStore]:
+        try:
+            with open(file, "rb") as f:
+                whole_file = f.read()
+                enc = chardet.detect(f.read())
+                lines = []
+                if enc["confidence"] > 0.9:
+                    encoding = enc.get("encoding")
+                    decoded = whole_file.decode(encoding.lower()) if encoding else ""
+                    lines = decoded.splitlines() if decoded else []
+                else:
+                    raise UnicodeError()
+                return PackageStore(
+                    type=self.file_name,
+                    file=str(file),
+                    dependencies=set(self._parse_dependencies(lines)),
+                    # requirements.txt files do not declare py versions explicitly
+                    # though we could create a heuristic by analyzing each dependency
+                    # and extracting py versions from them.
+                    py_versions=[],
+                )
+        except (UnicodeError, OSError):
+            logger.debug("Error parsing file: %s", file)
+        return None
