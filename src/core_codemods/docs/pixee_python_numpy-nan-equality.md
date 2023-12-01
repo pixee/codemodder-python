c11a55486d32a0575ad8bc993498dd482ba21aa2
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-12-01 10:49:51 -0300

    Added tests and docs for numpy-nan-equality

---
+++ src/core_codemods/docs/pixee_python_numpy-nan-equality.md
@@ -0,0 +1,12 @@
+Comparisons against `numpy.nan` always results in `False`. Thus comparing an expression directly against `numpy.nan` is always unintended. The correct way to compare a value for `NaN` is to use the `numpy.isnan` function.
+
+Our changes look something like this:
+
+```diff
+import numpy as np
+
+a = np.nan
+-if a == np.nan:
++if np.isnan(a):
+    pass
+```
---
+++ tests/codemods/test_numpy_nan_equality.py
@@ -0,0 +1,75 @@
+from core_codemods.numpy_nan_equality import NumpyNanEquality
+from tests.codemods.base_codemod_test import BaseCodemodTest
+from textwrap import dedent
+
+
+class TestNumpyNanEquality(BaseCodemodTest):
+    codemod = NumpyNanEquality
+
+    def test_name(self):
+        assert self.codemod.name() == "numpy-nan-equality"
+
+    def test_simple(self, tmpdir):
+        input_code = """\
+        import numpy
+        if a == numpy.nan:
+            pass
+        """
+        expected = """\
+        import numpy
+        if numpy.isnan(a):
+            pass
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(expected))
+        assert len(self.file_context.codemod_changes) == 1
+
+    def test_alias(self, tmpdir):
+        input_code = """\
+        import numpy as np
+        if a == np.nan:
+            pass
+        """
+        expected = """\
+        import numpy as np
+        if np.isnan(a):
+            pass
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(expected))
+        assert len(self.file_context.codemod_changes) == 1
+
+    def test_multiple_comparisons(self, tmpdir):
+        input_code = """\
+        import numpy as np
+        if a == np.nan == b:
+            pass
+        """
+        expected = """\
+        import numpy as np
+        if np.isnan(a) and np.isnan(b):
+            pass
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(expected))
+        assert len(self.file_context.codemod_changes) == 1
+
+    def test_multiple_comparisons_preserve_longest(self, tmpdir):
+        input_code = """\
+        import numpy as np
+        if a == np.nan == b == c == d <= e:
+            pass
+        """
+        expected = """\
+        import numpy as np
+        if np.isnan(a) and np.isnan(b) and b == c == d <= e:
+            pass
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(expected))
+        assert len(self.file_context.codemod_changes) == 1
+
+    def test_not_numpy(self, tmpdir):
+        input_code = """\
+        import not_numpy as np
+        if a == np.nan:
+            pass
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(input_code))
+        assert len(self.file_context.codemod_changes) == 0
