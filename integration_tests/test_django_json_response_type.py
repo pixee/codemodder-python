fce090187309b1ac4507d7c9bbc3371b855a0b33
andrecs <12188364+andrecsilva@users.noreply.github.com>
2023-12-04 10:55:00 -0300

    Tests and documentation for django-json-response-type

---
+++ integration_tests/test_django_json_response_type.py
@@ -0,0 +1,36 @@
+from core_codemods.django_json_response_type import DjangoJsonResponseType
+from integration_tests.base_test import (
+    BaseIntegrationTest,
+    original_and_expected_from_code_path,
+)
+
+
+class TestDjangoJsonResponseType(BaseIntegrationTest):
+    codemod = DjangoJsonResponseType
+    code_path = "tests/samples/django_json_response_type.py"
+    original_code, expected_new_code = original_and_expected_from_code_path(
+        code_path,
+        [
+            (
+                5,
+                """    return HttpResponse(json_response, content_type = "application/json")\n""",
+            ),
+        ],
+    )
+
+    # fmt: off
+    expected_diff =(
+    """--- \n"""
+    """+++ \n"""
+    """@@ -3,4 +3,4 @@\n"""
+    """ \n"""
+    """ def foo(request):\n"""
+    """     json_response = json.dumps({ "user_input": request.GET.get("input") })\n"""
+    """-    return HttpResponse(json_response)\n"""
+    """+    return HttpResponse(json_response, content_type = "application/json")\n"""
+    )
+    # fmt: on
+
+    expected_line_change = "6"
+    change_description = DjangoJsonResponseType.CHANGE_DESCRIPTION
+    num_changed_files = 1
---
+++ tests/codemods/test_django_json_response_type.py
@@ -0,0 +1,92 @@
+from core_codemods.django_json_response_type import DjangoJsonResponseType
+from tests.codemods.base_codemod_test import BaseSemgrepCodemodTest
+from textwrap import dedent
+
+
+class TestDjangoJsonResponseType(BaseSemgrepCodemodTest):
+    codemod = DjangoJsonResponseType
+
+    def test_name(self):
+        assert self.codemod.name() == "django-json-response-type"
+
+    def test_simple(self, tmpdir):
+        input_code = """\
+        from django.http import HttpResponse
+        import json
+
+        def foo(request):
+            json_response = json.dumps({ "user_input": request.GET.get("input") })
+            return HttpResponse(json_response)
+        """
+        expected = """\
+        from django.http import HttpResponse
+        import json
+
+        def foo(request):
+            json_response = json.dumps({ "user_input": request.GET.get("input") })
+            return HttpResponse(json_response, content_type = "application/json")
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(expected))
+        assert len(self.file_context.codemod_changes) == 1
+
+    def test_alias(self, tmpdir):
+        input_code = """\
+        from django.http import HttpResponse as response
+        import json as jsan
+
+        def foo(request):
+            json_response = jsan.dumps({ "user_input": request.GET.get("input") })
+            return response(json_response)
+        """
+        expected = """\
+        from django.http import HttpResponse as response
+        import json as jsan
+
+        def foo(request):
+            json_response = jsan.dumps({ "user_input": request.GET.get("input") })
+            return response(json_response, content_type = "application/json")
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(expected))
+        assert len(self.file_context.codemod_changes) == 1
+
+    def test_direct(self, tmpdir):
+        input_code = """\
+        from django.http import HttpResponse
+        import json
+
+        def foo(request):
+            return HttpResponse(json.dumps({ "user_input": request.GET.get("input") }))
+        """
+        expected = """\
+        from django.http import HttpResponse
+        import json
+
+        def foo(request):
+            return HttpResponse(json.dumps({ "user_input": request.GET.get("input") }), content_type = "application/json")
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(expected))
+        assert len(self.file_context.codemod_changes) == 1
+
+    def test_content_type_set(self, tmpdir):
+        input_code = """\
+        from django.http import HttpResponse
+        import json
+
+        def foo(request):
+            json_response = json.dumps({ "user_input": request.GET.get("input") })
+            return HttpResponse(json_response, content_type='application/json')
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(input_code))
+        assert len(self.file_context.codemod_changes) == 0
+
+    def test_no_json_input(self, tmpdir):
+        input_code = """\
+        from django.http import HttpResponse
+        import json
+
+        def foo(request):
+            dict_reponse = { "user_input": request.GET.get("input") }
+            return HttpResponse(dict_response)
+        """
+        self.run_and_assert(tmpdir, dedent(input_code), dedent(input_code))
+        assert len(self.file_context.codemod_changes) == 0
