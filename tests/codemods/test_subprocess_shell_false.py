751d1a0835a2a2e6a63df8a0ee787ebf50022cf1
clavedeluna <danalitovsky+git@gmail.com>
2024-01-02 16:47:20 -0300

    add review updates

--- tests/codemods/test_subprocess_shell_false.py
+++ tests/codemods/test_subprocess_shell_false.py
@@ -15,40 +15,44 @@ class TestSubprocessShellFalse(BaseCodemodTest):
 
     @each_func
     def test_import(self, tmpdir, func):
-        input_code = f"""import subprocess
-subprocess.{func}(args, shell=True)
-"""
-        expexted_output = f"""import subprocess
-subprocess.{func}(args, shell=False)
-"""
-
+        input_code = f"""
+        import subprocess
+        subprocess.{func}(args, shell=True)
+        """
+        expexted_output = f"""
+        import subprocess
+        subprocess.{func}(args, shell=False)
+        """
         self.run_and_assert(tmpdir, input_code, expexted_output)
         assert len(self.file_context.codemod_changes) == 1
 
     @each_func
     def test_from_import(self, tmpdir, func):
-        input_code = f"""from subprocess import {func}
-{func}(args, shell=True)
-"""
-        expexted_output = f"""from subprocess import {func}
-{func}(args, shell=False)
-"""
-
+        input_code = f"""
+        from subprocess import {func}
+        {func}(args, shell=True)
+        """
+        expexted_output = f"""
+        from subprocess import {func}
+        {func}(args, shell=False)
+        """
         self.run_and_assert(tmpdir, input_code, expexted_output)
         assert len(self.file_context.codemod_changes) == 1
 
     @each_func
     def test_no_shell(self, tmpdir, func):
-        input_code = f"""import subprocess
-subprocess.{func}(args, timeout=1)
-"""
+        input_code = f"""
+        import subprocess
+        subprocess.{func}(args, timeout=1)
+        """
         self.run_and_assert(tmpdir, input_code, input_code)
         assert len(self.file_context.codemod_changes) == 0
 
     @each_func
     def test_shell_False(self, tmpdir, func):
-        input_code = f"""import subprocess
-subprocess.{func}(args, shell=False)
-"""
+        input_code = f"""
+        import subprocess
+        subprocess.{func}(args, shell=False)
+        """
         self.run_and_assert(tmpdir, input_code, input_code)
         assert len(self.file_context.codemod_changes) == 0
