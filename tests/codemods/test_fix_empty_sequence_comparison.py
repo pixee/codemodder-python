302b1cf5e543b9605917c7f558ded6d7c0df6e0a
clavedeluna <danalitovsky+git@gmail.com>
2024-01-11 09:56:23 -0300

    refactor and add xfail

--- tests/codemods/test_fix_empty_sequence_comparison.py
+++ tests/codemods/test_fix_empty_sequence_comparison.py
@@ -3,7 +3,7 @@ from core_codemods.fix_empty_sequence_comparison import FixEmptySequenceComparis
 from tests.codemods.base_codemod_test import BaseCodemodTest
 
 
-class TestFixEmptySequenceComparison(BaseCodemodTest):
+class TestFixEmptySequenceComparisonIfStatements(BaseCodemodTest):
     codemod = FixEmptySequenceComparison
 
     @pytest.mark.parametrize(
@@ -69,7 +69,7 @@ class TestFixEmptySequenceComparison(BaseCodemodTest):
             ),
         ],
     )
-    def test_change_list_if_statements(self, tmpdir, input_code, expected_output):
+    def test_change_list(self, tmpdir, input_code, expected_output):
         self.run_and_assert(tmpdir, input_code, expected_output)
         assert len(self.file_context.codemod_changes) == 1
 
@@ -136,7 +136,7 @@ class TestFixEmptySequenceComparison(BaseCodemodTest):
             ),
         ],
     )
-    def test_change_dict_if_statements(self, tmpdir, input_code, expected_output):
+    def test_change_dict(self, tmpdir, input_code, expected_output):
         self.run_and_assert(tmpdir, input_code, expected_output)
         assert len(self.file_context.codemod_changes) == 1
 
@@ -203,87 +203,89 @@ class TestFixEmptySequenceComparison(BaseCodemodTest):
             ),
         ],
     )
-    def test_change_tuple_if_statements(self, tmpdir, input_code, expected_output):
+    def test_change_tuple(self, tmpdir, input_code, expected_output):
+        self.run_and_assert(tmpdir, input_code, expected_output)
+        assert len(self.file_context.codemod_changes) == 1
+
+    @pytest.mark.parametrize(
+        "code",
+        [
+            """
+            x = [1]
+            if x == "hi":
+                pass
+            """,
+            """
+            x = [1]
+            if x is [1]:
+                pass
+            """,
+        ],
+    )
+    def test_no_change(self, tmpdir, code):
+        self.run_and_assert(tmpdir, code, code)
+        assert len(self.file_context.codemod_changes) == 0
+
+
+@pytest.mark.xfail(
+    reason="Not yet support changing multiple comparisons in a statement"
+)
+class TestFixEmptySequenceComparisonMultipleStatements(BaseCodemodTest):
+    codemod = FixEmptySequenceComparison
+
+    @pytest.mark.parametrize(
+        "input_code,expected_output",
+        [
+            (
+                """
+            x = [1, 2]
+            y = [3, 4]
+            if x != [] or y == []:
+                pass
+            """,
+                """
+            if x or not y:
+                pass
+            """,
+            ),
+        ],
+    )
+    def test_change(self, tmpdir, input_code, expected_output):
         self.run_and_assert(tmpdir, input_code, expected_output)
         assert len(self.file_context.codemod_changes) == 1
 
 
-# no change
-# if x == 'hi', x is [],
-# @pytest.mark.parametrize(
-#     "code",
-#     [
-#         """
-#     from logging import {0}
-#     {0}('something')
-#     """,
-#         """
-#     from logging import getLogger
-#     getLogger('anything').{0}('something')
-#     """,
-#     ],
-# )
-# def test_from_import(self, tmpdir, code):
-#     original_code = code.format("warn")
-#     new_code = code.format("warning")
-#     self.run_and_assert(tmpdir, original_code, new_code)
-#     assert len(self.file_context.codemod_changes) == 1
-#
-# @pytest.mark.parametrize(
-#     "input_code,expected_output",
-#     [
-#         (
-#             """\
-#             from logging import warn as warn_func
-#             warn_func('something')""",
-#             """\
-#             from logging import warning
-#             warning('something')""",
-#         ),
-#         (
-#             """\
-#             from logging import getLogger as make_logger
-#             logger = make_logger('anything')
-#             logger.warn('something')""",
-#             """\
-#             from logging import getLogger as make_logger
-#             logger = make_logger('anything')
-#             logger.warning('something')""",
-#         ),
-#     ],
-# )
-# def test_import_alias(self, tmpdir, input_code, expected_output):
-#     self.run_and_assert(tmpdir, input_code, expected_output)
-#     assert len(self.file_context.codemod_changes) == 1
-#
-# @pytest.mark.parametrize(
-#     "code",
-#     [
-#         """
-#     import xyz
-#     xyz.warn('something')
-#     """,
-#         """
-#     import my_logging
-#     log = my_logging.getLogger('anything')
-#     log.warn('something')
-#     """,
-#     ],
-# )
-# def test_different_warn(self, tmpdir, code):
-#     self.run_and_assert(tmpdir, code, code)
-#     assert len(self.file_context.codemod_changes) == 0
-#
-# @pytest.mark.xfail(reason="Not currently supported")
-# def test_log_as_arg(self, tmpdir):
-#     code = """\
-#     import logging
-#     log = logging.getLogger('foo')
-#     def some_function(logger):
-#         logger.()("hi")
-#     some_function(log)
-#     """
-#     original_code = code.format("warn")
-#     new_code = code.format("warning")
-#     self.run_and_assert(tmpdir, original_code, new_code)
-#     assert len(self.file_context.codemod_changes) == 1
+@pytest.mark.xfail(
+    reason="Not yet support changing comparisons in assignment statements."
+)
+class TestFixEmptySequenceComparisonAssignmentStatements(BaseCodemodTest):
+    codemod = FixEmptySequenceComparison
+
+    @pytest.mark.parametrize(
+        "input_code,expected_output",
+        [
+            (
+                """
+            x = [1, 2]
+            res = x == []
+            """,
+                """
+            x = [1, 2]
+            res = not x
+            """,
+            ),
+            (
+                """
+            x = [1, 2]
+            res = x != []
+            """,
+                """
+            x = [1, 2]
+            res = bool(x)
+            """,
+            ),
+        ],
+    )
+    def test_change(self, tmpdir, input_code, expected_output):
+        self.run_and_assert(tmpdir, input_code, expected_output)
+        assert len(self.file_context.codemod_changes) == 1
