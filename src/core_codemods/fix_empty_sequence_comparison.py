9d48514c406744ac69e0662145e9a8cdd85f035e
clavedeluna <danalitovsky+git@gmail.com>
2024-01-12 09:46:35 -0300

    can simplify empty check for assert statements

--- src/core_codemods/fix_empty_sequence_comparison.py
+++ src/core_codemods/fix_empty_sequence_comparison.py
@@ -13,9 +13,47 @@ class FixEmptySequenceComparison(
     DESCRIPTION = "TODO"
     REFERENCES: list = []
 
-    def leave_If(
-        self, original_node: cst.If, updated_node: cst.If
-    ) -> cst.BaseStatement:
+    def leave_If(self, original_node: cst.If, updated_node: cst.If) -> cst.If:
+        return self._simplify_empty_check(original_node, updated_node)
+
+    def leave_Assert(
+        self, original_node: cst.Assert, updated_node: cst.Assert
+    ) -> cst.Assert:
+        if not self.filter_by_path_includes_or_excludes(
+            self.node_position(original_node)
+        ):
+            return original_node
+
+        match original_node.test:
+            case cst.Comparison(
+                left=left, comparisons=[cst.ComparisonTarget() as target]
+            ):
+                if isinstance(target.operator, cst.Equal | cst.NotEqual):
+                    right = target.comparator
+                    # right is empty: x == []
+                    # left is empty: [] == x
+                    if (
+                        empty_left := self._is_empty_sequence(left)
+                    ) or self._is_empty_sequence(right):
+                        self.report_change(original_node)
+                        if isinstance(target.operator, cst.NotEqual):
+                            return updated_node.with_changes(
+                                test=right if empty_left else left,
+                            )
+                        return updated_node.with_changes(
+                            test=cst.UnaryOperation(
+                                operator=cst.Not(),
+                                expression=right if empty_left else left,
+                            )
+                        )
+        return original_node
+        return self._simplify_empty_check(original_node, updated_node)
+
+    def _simplify_empty_check(
+        self,
+        original_node: Union[cst.Assert, cst.If],
+        updated_node: Union[cst.Assert, cst.If],
+    ) -> Union[cst.Assert, cst.If]:
         if not self.filter_by_path_includes_or_excludes(
             self.node_position(original_node)
         ):
@@ -50,16 +88,3 @@ class FixEmptySequenceComparison(
             case cst.List(elements=[]) | cst.Dict(elements=[]) | cst.Tuple(elements=[]):
                 return True
         return False
-
-    # def leave_Assert(self, original_node: cst.Assert, updated_node: cst.Assert) -> cst.BaseSmallStatement:
-    #     #filter
-    #     return self._simplify_empty_check(updated_node)
-
-    # def _simplify_empty_check(self, node): # add type
-
-    # if isinstance(node.test, cst.BooleanOperation) and isinstance(node.test.operator, cst.And):
-    #     new_left = self._simplify_comparison(node.test.left)
-    #     new_right = self._simplify_comparison(node.test.right)
-    #     return node.with_changes(test=cst.BooleanOperation(left=new_left, operator=cst.And(), right=new_right))
-
-    # return self._simplify_comparison(node)
