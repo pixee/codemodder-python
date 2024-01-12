9eff4d4ad802990411d374f79c291e1978106ed8
clavedeluna <danalitovsky+git@gmail.com>
2024-01-12 09:47:15 -0300

    can simplify empty check for assert statements

--- src/core_codemods/fix_empty_sequence_comparison.py
+++ src/core_codemods/fix_empty_sequence_comparison.py
@@ -19,34 +19,6 @@ class FixEmptySequenceComparison(
     def leave_Assert(
         self, original_node: cst.Assert, updated_node: cst.Assert
     ) -> cst.Assert:
-        if not self.filter_by_path_includes_or_excludes(
-            self.node_position(original_node)
-        ):
-            return original_node
-
-        match original_node.test:
-            case cst.Comparison(
-                left=left, comparisons=[cst.ComparisonTarget() as target]
-            ):
-                if isinstance(target.operator, cst.Equal | cst.NotEqual):
-                    right = target.comparator
-                    # right is empty: x == []
-                    # left is empty: [] == x
-                    if (
-                        empty_left := self._is_empty_sequence(left)
-                    ) or self._is_empty_sequence(right):
-                        self.report_change(original_node)
-                        if isinstance(target.operator, cst.NotEqual):
-                            return updated_node.with_changes(
-                                test=right if empty_left else left,
-                            )
-                        return updated_node.with_changes(
-                            test=cst.UnaryOperation(
-                                operator=cst.Not(),
-                                expression=right if empty_left else left,
-                            )
-                        )
-        return original_node
         return self._simplify_empty_check(original_node, updated_node)
 
     def _simplify_empty_check(
