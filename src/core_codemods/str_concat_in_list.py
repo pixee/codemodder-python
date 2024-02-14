eee118c0dbde293022810852daa5febf39ed5f14
clavedeluna <danalitovsky+git@gmail.com>
2024-02-14 12:05:26 -0300

    fully working codemod for str concat in list

--- src/core_codemods/str_concat_in_list.py
+++ src/core_codemods/str_concat_in_list.py
@@ -19,33 +19,39 @@ class StrConcatInList(SimpleCodemod, NameResolutionMixin, AncestorPatternsMixin)
     )
 
     def leave_List(self, original_node: cst.List, updated_node: cst.List) -> cst.List:
-        new_elements = self.process_elements(original_node)
-        return updated_node.with_changes(elements=new_elements)
+        if not self.filter_by_path_includes_or_excludes(
+            self.node_position(original_node)
+        ):
+            return updated_node
+        return updated_node.with_changes(elements=self.process_elements(original_node))
 
-    def process_elements(self, original_node):
+    def process_elements(self, original_node: cst.List) -> list[cst.Element]:
         new_elements = []
         prev_comma = None
-        for idx, element in enumerate(original_node.elements):
-            if isinstance(element.value, cst.ConcatenatedString):
-                flattened_parts = self.flatten_concatenated_strings(element.value)
-                for part in flattened_parts:
-                    # the very last element should only have a comma if the last element
-                    # of the original list had a comma
-                    if (
-                        element == original_node.elements[-1]
-                        and part == flattened_parts[-1]
-                    ):
-                        new_elements.append(
-                            cst.Element(value=part, comma=element.comma)
-                        )
-                    else:
-                        new_elements.append(
-                            cst.Element(value=part, comma=prev_comma or element.comma)
-                        )
+        for element in original_node.elements:
+            match element.value:
+                case cst.ConcatenatedString():
                     self.report_change(original_node)
-            else:
-                prev_comma = element.comma
-                new_elements.append(element)
+                    flattened_parts = self.flatten_concatenated_strings(element.value)
+                    for part in flattened_parts:
+                        # the very last element should only have a comma if the last element
+                        # of the original list had a comma
+                        if (
+                            element == original_node.elements[-1]
+                            and part == flattened_parts[-1]
+                        ):
+                            new_elements.append(
+                                cst.Element(value=part, comma=element.comma)
+                            )
+                        else:
+                            new_elements.append(
+                                cst.Element(
+                                    value=part, comma=prev_comma or element.comma
+                                )
+                            )
+                case _:
+                    prev_comma = element.comma
+                    new_elements.append(element)
         return new_elements
 
     def flatten_concatenated_strings(
@@ -53,12 +59,11 @@ class StrConcatInList(SimpleCodemod, NameResolutionMixin, AncestorPatternsMixin)
     ):
         if parts is None:
             parts = []
-        if isinstance(concat_node.left, cst.ConcatenatedString):
-            self.flatten_concatenated_strings(concat_node.left, parts)
-        else:
-            parts.append(concat_node.left)
-        if isinstance(concat_node.right, cst.ConcatenatedString):
-            self.flatten_concatenated_strings(concat_node.right, parts)
-        else:
-            parts.append(concat_node.right)
+
+        for node in concat_node.left, concat_node.right:
+            match node:
+                case cst.ConcatenatedString():
+                    self.flatten_concatenated_strings(node, parts)
+                case _:
+                    parts.append(node)
         return parts
