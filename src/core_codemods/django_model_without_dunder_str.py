2aa8ad77a756523353bed26d3ab8346d69ac935d
clavedeluna <danalitovsky+git@gmail.com>
2024-02-21 15:14:00 -0300

    initial django dunder str codmeod

---
+++ src/core_codemods/django_model_without_dunder_str.py
@@ -0,0 +1,70 @@
+from typing import Union
+import libcst as cst
+from codemodder.codemods.libcst_transformer import (
+    LibcstResultTransformer,
+    LibcstTransformerPipeline,
+)
+from codemodder.codemods.utils_mixin import NameResolutionMixin
+from core_codemods.api import (
+    Metadata,
+    Reference,
+    ReviewGuidance,
+)
+from core_codemods.api.core_codemod import CoreCodemod
+
+
+class DjangoModelWithoutDunderStrTransformer(
+    LibcstResultTransformer, NameResolutionMixin
+):
+    change_description = "todoMoved @receiver to the top."
+
+    def leave_ClassDef(
+        self, original_node: cst.ClassDef, updated_node: cst.ClassDef
+    ) -> Union[
+        cst.BaseStatement, cst.FlattenSentinel[cst.BaseStatement], cst.RemovalSentinel
+    ]:
+
+        # TODO: add filter by include or exclude that works for nodes
+        # that that have different start/end numbers.
+        if not any(
+            self.find_base_name(base.value) == "django.db.models.Model"
+            for base in original_node.bases
+        ):
+            return updated_node
+
+        if self.implements_dunder_str(original_node):
+            return updated_node
+
+        self.report_change(original_node)
+        dunder_str = cst.FunctionDef(
+            # leading_lines=[cst.EmptyLine()],
+            name=cst.Name("__str__"),
+            params=cst.Parameters(params=[cst.Param(name=cst.Name("self"))]),
+            body=cst.IndentedBlock(body=[cst.SimpleStatementLine(body=[cst.Pass()])]),
+        )
+        new_body = updated_node.body.with_changes(
+            body=[*updated_node.body.body, dunder_str]
+        )
+
+        return updated_node.with_changes(body=new_body)
+
+    def implements_dunder_str(self, original_node: cst.ClassDef) -> bool:
+        for node in original_node.body.body:
+            match node:
+                case cst.FunctionDef(name=cst.Name(value="__str__")):
+                    return True
+        return False
+
+
+DjangoModelWithoutDunderStr = CoreCodemod(
+    metadata=Metadata(
+        name="django-model-without-dunder-str",
+        summary="TODOEnsure Django @receiver is the first decorator",
+        review_guidance=ReviewGuidance.MERGE_WITHOUT_REVIEW,
+        references=[
+            Reference(url="todohttps://docs.djangoproject.com/en/4.1/topics/signals/"),
+        ],
+    ),
+    transformer=LibcstTransformerPipeline(DjangoModelWithoutDunderStrTransformer),
+    detector=None,
+)
---
+++ tests/codemods/test_django_model_without_dunder_str.py
@@ -0,0 +1,104 @@
+from core_codemods.django_model_without_dunder_str import DjangoModelWithoutDunderStr
+from tests.codemods.base_codemod_test import BaseCodemodTest
+
+
+class TestDjangoModelWithoutDunderStr(BaseCodemodTest):
+    codemod = DjangoModelWithoutDunderStr
+
+    def test_name(self):
+        assert self.codemod.name == "django-model-without-dunder-str"
+
+    def test_no_change(self, tmpdir):
+        input_code = """
+        from django.db import models
+
+        class User(models.Model):
+            name = models.CharField(max_length=100)
+            phone = models.IntegerField(blank=True)
+
+            def __str__(self):
+                return "doesntmatter"
+        """
+        self.run_and_assert(tmpdir, input_code, input_code)
+
+    def test_no_dunder_str(self, tmpdir):
+        input_code = """
+        from django.db import models
+
+        class User(models.Model):
+            name = models.CharField(max_length=100)
+            phone = models.IntegerField(blank=True)
+            
+            @property
+            def decorated_name(self):
+                return f"***{self.name}***"
+
+        def something():
+            pass
+        """
+        expected = """
+        from django.db import models
+
+        class User(models.Model):
+            name = models.CharField(max_length=100)
+            phone = models.IntegerField(blank=True)
+
+            @property
+            def decorated_name(self):
+                return f"***{self.name}***"
+            def __str__(self):
+                pass
+        
+        def something():
+            pass
+        """
+        self.run_and_assert(tmpdir, input_code, expected)
+
+    # def test_simple_alias(self, tmpdir):
+    #     input_code = """
+    #     from django.dispatch import receiver as rec
+    #
+    #     @csrf_exempt
+    #     @rec(request_finished)
+    #     def foo():
+    #         pass
+    #     """
+    #     expected = """
+    #     from django.dispatch import receiver as rec
+    #
+    #     @rec(request_finished)
+    #     @csrf_exempt
+    #     def foo():
+    #         pass
+    #     """
+    #     self.run_and_assert(tmpdir, input_code, expected)
+    #
+    # def test_no_receiver(self, tmpdir):
+    #     input_code = """
+    #     @csrf_exempt
+    #     def foo():
+    #         pass
+    #     """
+    #     self.run_and_assert(tmpdir, input_code, input_code)
+    #
+    # def test_receiver_but_not_djangos(self, tmpdir):
+    #     input_code = """
+    #     from not_django import receiver
+    #
+    #     @csrf_exempt
+    #     @receiver(request_finished)
+    #     def foo():
+    #         pass
+    #     """
+    #     self.run_and_assert(tmpdir, input_code, input_code)
+    #
+    # def test_receiver_on_top(self, tmpdir):
+    #     input_code = """
+    #     from django.dispatch import receiver
+    #
+    #     @receiver(request_finished)
+    #     @csrf_exempt
+    #     def foo():
+    #         pass
+    #     """
+    #     self.run_and_assert(tmpdir, input_code, input_code)
