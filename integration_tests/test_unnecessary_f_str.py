ceeb483866d5fe7d1ad670ddb71b4fc5a29590ca
andrecs <12188364+andrecsilva@users.noreply.github.com>
2024-03-12 11:05:18 -0300

    fixup! Refactoring and more tests

--- integration_tests/test_unnecessary_f_str.py
+++ integration_tests/test_unnecessary_f_str.py
@@ -2,7 +2,10 @@ from codemodder.codemods.test import (
     BaseIntegrationTest,
     original_and_expected_from_code_path,
 )
-from core_codemods.remove_unnecessary_f_str import RemoveUnnecessaryFStr
+from core_codemods.remove_unnecessary_f_str import (
+    RemoveUnnecessaryFStr,
+    RemoveUnnecessaryFStrTransform,
+)
 
 
 class TestFStr(BaseIntegrationTest):
@@ -13,4 +16,4 @@ class TestFStr(BaseIntegrationTest):
     )
     expected_diff = '--- \n+++ \n@@ -1,2 +1,2 @@\n-bad = f"hello"\n+bad = "hello"\n good = f"{2+3}"\n'
     expected_line_change = "1"
-    change_description = RemoveUnnecessaryFStr.change_description
+    change_description = RemoveUnnecessaryFStrTransform.change_description
