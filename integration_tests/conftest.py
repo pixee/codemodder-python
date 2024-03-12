7e0747f9d5d095389d802f64321400dac26e621f
Dan D'Avella <dan.davella@pixee.ai>
2024-03-12 14:35:43 -0400

    Use session scope for schema validation fixture (#363)

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 import requests
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def codetf_schema():
     schema_path = "https://raw.githubusercontent.com/pixee/codemodder-specs/main/codetf.schema.json"
     response = requests.get(schema_path, timeout=60)
