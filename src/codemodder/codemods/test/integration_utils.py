a6c754ac210a6a8e8c2fc5c944ce9a39940eead5
clavedeluna <danalitovsky+git@gmail.com>
2024-03-20 12:16:55 -0300

    start making int tests independent"

--- src/codemodder/codemods/test/integration_utils.py
+++ src/codemodder/codemods/test/integration_utils.py
@@ -2,19 +2,19 @@ import json
 import os
 import pathlib
 import subprocess
-import sys
+import tempfile
+from textwrap import dedent
 from types import ModuleType
 
-import git
 import jsonschema
 
 from codemodder import __version__, registry
 
 from .validations import execute_code
 
-SAMPLES_DIR = "tests/samples"
-# Enable import of test modules from test directory
-sys.path.append(SAMPLES_DIR)
+# SAMPLES_DIR = "tests/samples"
+# # Enable import of test modules from test directory
+# sys.path.append(SAMPLES_DIR)
 
 
 class CleanRepoMixin:
@@ -22,10 +22,11 @@ class CleanRepoMixin:
     def teardown_class(cls):
         """Ensure any re-written file is undone after integration test class"""
         # TODO: we should probably use temporary directories instead
-        repo = git.Git(os.getcwd())
-        repo.execute(["git", "checkout", os.path.join(os.getcwd(), SAMPLES_DIR)])
-
-        pathlib.Path(cls.output_path).unlink(missing_ok=True)
+        # repo = git.Git(os.getcwd())
+        # repo.execute(["git", "checkout", os.path.join(os.getcwd(), SAMPLES_DIR)])
+        # pathlib.Path(cls.output_path).unlink(missing_ok=True)
+        #
+        # os.remove(path)
 
 
 class DependencyTestMixin:
@@ -49,10 +50,8 @@ class DependencyTestMixin:
 
 class BaseIntegrationTest(DependencyTestMixin, CleanRepoMixin):
     codemod = NotImplementedError
-    code_path = NotImplementedError
     original_code = NotImplementedError
     expected_new_code = NotImplementedError
-    output_path = "test-codetf.txt"
     num_changes = 1
     _lines: list = []
     num_changed_files = 1
@@ -62,6 +61,12 @@ class BaseIntegrationTest(DependencyTestMixin, CleanRepoMixin):
     @classmethod
     def setup_class(cls):
         cls.codemod_registry = registry.load_registered_codemods()
+
+        # todo: can supply dir to all
+        cls.code_path = tempfile.mkstemp(suffix=".py")[1]
+        cls.code_dir = os.path.dirname(cls.code_path)
+        cls.code_filename = os.path.relpath(cls.code_path, cls.code_dir)
+        cls.output_path = tempfile.mkstemp()[1]
 
     def setup_method(self):
         try:
@@ -80,19 +85,18 @@ class BaseIntegrationTest(DependencyTestMixin, CleanRepoMixin):
             ) from exc
 
     def _assert_run_fields(self, run, output_path):
-        code_path = os.path.relpath(self.code_path, SAMPLES_DIR)
         assert run["vendor"] == "pixee"
         assert run["tool"] == "codemodder-python"
         assert run["version"] == __version__
         assert run["elapsed"] != ""
         assert run[
             "commandLine"
-        ] == f'codemodder {SAMPLES_DIR} --output {output_path} --codemod-include={self.codemod_instance.name} --path-include={code_path} --path-exclude=""' + (
+        ] == f'codemodder {self.code_dir} --output {output_path} --codemod-include={self.codemod_instance.name} --path-include={self.code_filename} --path-exclude=""' + (
             f" --sonar-issues-json={self.sonar_issues_json}"
             if self.sonar_issues_json
             else ""
         )
-        assert run["directory"] == os.path.abspath(SAMPLES_DIR)
+        assert run["directory"] == os.path.abspath(self.code_dir)
         assert run["sarifs"] == []
 
     def _assert_sonar_fields(self, result):
@@ -160,21 +164,18 @@ class BaseIntegrationTest(DependencyTestMixin, CleanRepoMixin):
         self._assert_run_fields(run, self.output_path)
         results = codetf["results"]
         # CodeTf2 spec requires relative paths
-        self._assert_results_fields(
-            results, os.path.relpath(self.code_path, SAMPLES_DIR)
-        )
+        self._assert_results_fields(results, self.code_filename)
 
-    def check_code_before(self):
-        with open(self.code_path, "r", encoding="utf-8") as f:
-            code = f.read()
-        assert code == self.original_code
+    def write_original_code(self):
+        with open(self.code_path, "w", encoding="utf-8") as f:  # type: ignore
+            f.write(self.original_code)
 
     def check_code_after(self) -> ModuleType:
         with open(self.code_path, "r", encoding="utf-8") as f:  # type: ignore
             new_code = f.read()
         assert new_code == self.expected_new_code
         return execute_code(
-            path=self.code_path, allowed_exceptions=self.allowed_exceptions
+            path=self.code_path, allowed_exceptions=self.allowed_exceptions  # type: ignore
         )
 
     def test_file_rewritten(self, codetf_schema):
@@ -185,22 +186,22 @@ class BaseIntegrationTest(DependencyTestMixin, CleanRepoMixin):
         Mocks won't work when making a subprocess call so make sure to delete all
         output files
         """
-        code_path = os.path.relpath(self.code_path, SAMPLES_DIR)
         command = [
             "codemodder",
-            SAMPLES_DIR,
+            self.code_dir,
             "--output",
             self.output_path,
             f"--codemod-include={self.codemod_instance.name}",
-            f"--path-include={code_path}",
+            f"--path-include={self.code_filename}",
             '--path-exclude=""',
         ]
 
         if self.sonar_issues_json:
             command.append(f"--sonar-issues-json={self.sonar_issues_json}")
 
-        self.check_code_before()
-        self.check_dependencies_before()
+        self.write_original_code()
+        # todo: enable
+        # self.check_dependencies_before()
 
         completed_process = subprocess.run(
             command,
@@ -210,12 +211,12 @@ class BaseIntegrationTest(DependencyTestMixin, CleanRepoMixin):
         assert completed_process.returncode == 0
 
         self.check_code_after()
-        self.check_dependencies_after()
+        # self.check_dependencies_after()
         self._assert_codetf_output(codetf_schema)
-        pathlib.Path(self.output_path).unlink(missing_ok=True)
-        self._run_idempotency_chec(command)
+        # pathlib.Path(self.output_path).unlink(missing_ok=True)
+        # self._run_idempotency_check(command)
 
-    def _run_idempotency_chec(self, command):
+    def _run_idempotency_check(self, command):
         # idempotency test, run it again and assert no files changed
         completed_process = subprocess.run(
             command,
@@ -234,26 +235,22 @@ class BaseIntegrationTest(DependencyTestMixin, CleanRepoMixin):
         assert still_new_code == self.expected_new_code
 
 
-def original_and_expected_from_code_path(code_path, replacements):
+def original_and_expected_from_code_path(original_code, replacements):
     """
     Returns a pair (original_code, expected) where original_code contains the contents of the code_path file and expected contains the code_path file where, for each (i,replacement) in replacements, the lines numbered i in original_code are replaced with replacement.
     """
-    lines = _lines_from_codepath(code_path)
-    original_code = "".join(lines)
+    original_code = dedent(original_code).strip("\n")
+    lines = original_code.split("\n")
+    lines = [line + "\n" for line in lines]
     _replace_lines_with(lines, replacements)
-    return (original_code, "".join(lines))
-
-
-def _lines_from_codepath(code_path):
-    with open(code_path, mode="r", encoding="utf-8") as f:
-        return f.readlines()
+    return (original_code + "\n", "".join(lines))
 
 
 def _replace_lines_with(lines, replacements):
     total_lines = len(lines)
     for lineno, replacement in replacements:
-        if lineno >= total_lines:
+        if lineno >= total_lines + 1:
             lines.extend(replacement)
             continue
-        lines[lineno] = replacement
+        lines[lineno - 1] = replacement
     return lines
