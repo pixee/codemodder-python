ed5750f3368c502be70fd59677e99cc05622b49a
clavedeluna <danalitovsky+git@gmail.com>
2024-03-21 08:43:13 -0300

    allow for providing expected_new_code instead of replacement_lines

--- integration_tests/test_with_threading_lock.py
+++ integration_tests/test_with_threading_lock.py
@@ -1,21 +1,20 @@
-from codemodder.codemods.test import (
-    BaseIntegrationTest,
-    original_and_expected_from_code_path,
-)
+from codemodder.codemods.test import BaseIntegrationTest
 from core_codemods.with_threading_lock import WithThreadingLock
 
 
 class TestWithThreadingLock(BaseIntegrationTest):
     codemod = WithThreadingLock
-    code_path = "tests/samples/with_threading_lock.py"
-    original_code, expected_new_code = original_and_expected_from_code_path(
-        code_path,
-        [
-            (1, "lock = threading.Lock()\n"),
-            (2, "with lock:\n"),
-            (4, '    print("Hello")\n'),
-        ],
-    )
+    original_code = """
+    import threading
+    with threading.Lock():
+        print("Hello")
+    """
+    replacement_lines = [
+        (2, "lock = threading.Lock()\n"),
+        (3, "with lock:\n"),
+        (5, '    print("Hello")\n'),
+    ]
+
     expected_diff = '--- \n+++ \n@@ -1,3 +1,4 @@\n import threading\n-with threading.Lock():\n+lock = threading.Lock()\n+with lock:\n     print("Hello")\n'
     expected_line_change = "2"
     change_description = WithThreadingLock.change_description
