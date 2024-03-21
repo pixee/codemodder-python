5826dafa55e1a91168309cb55d4eac37a936079e
clavedeluna <danalitovsky+git@gmail.com>
2024-03-21 08:34:24 -0300

    initial batch of fixed integration tests

--- integration_tests/test_add_requests_timeout.py
+++ integration_tests/test_add_requests_timeout.py
@@ -1,7 +1,6 @@
-from codemodder.codemods.test import (
-    BaseIntegrationTest,
-    original_and_expected_from_code_path,
-)
+from requests.exceptions import ConnectionError
+
+from codemodder.codemods.test import BaseIntegrationTest
 from core_codemods.add_requests_timeouts import (
     AddRequestsTimeouts,
     TransformAddRequestsTimeouts,
@@ -10,15 +9,19 @@ from core_codemods.add_requests_timeouts import (
 
 class TestAddRequestsTimeouts(BaseIntegrationTest):
     codemod = AddRequestsTimeouts
-    code_path = "tests/samples/requests_timeout.py"
+    original_code = """
+    import requests
+    
+    requests.get("https://example.com")
+    requests.get("https://example.com", timeout=1)
+    requests.get("https://example.com", timeout=(1, 10), verify=False)
+    requests.post("https://example.com", verify=False)
+    """
 
-    original_code, expected_new_code = original_and_expected_from_code_path(
-        code_path,
-        [
-            (2, 'requests.get("https://example.com", timeout=60)\n'),
-            (5, 'requests.post("https://example.com", verify=False, timeout=60)\n'),
-        ],
-    )
+    replacement_lines = [
+        (3, 'requests.get("https://example.com", timeout=60)\n'),
+        (6, 'requests.post("https://example.com", verify=False, timeout=60)\n'),
+    ]
 
     expected_diff = """\
 --- 
@@ -37,3 +40,5 @@ class TestAddRequestsTimeouts(BaseIntegrationTest):
     num_changes = 2
     expected_line_change = "3"
     change_description = TransformAddRequestsTimeouts.change_description
+    # expected because requests are made
+    allowed_exceptions = (ConnectionError,)
--- integration_tests/test_combine_startswith_endswith.py
+++ integration_tests/test_combine_startswith_endswith.py
@@ -1,16 +1,16 @@
-from codemodder.codemods.test import (
-    BaseIntegrationTest,
-    original_and_expected_from_code_path,
-)
+from codemodder.codemods.test import BaseIntegrationTest
 from core_codemods.combine_startswith_endswith import CombineStartswithEndswith
 
 
 class TestCombineStartswithEndswith(BaseIntegrationTest):
     codemod = CombineStartswithEndswith
-    code_path = "tests/samples/combine_startswith_endswith.py"
-    original_code, expected_new_code = original_and_expected_from_code_path(
-        code_path, [(1, 'if x.startswith(("foo", "bar")):\n')]
-    )
+    original_code = """
+    x = 'foo'
+    if x.startswith("foo") or x.startswith("bar"):
+        print("Yes")
+    """
+    replacement_lines = [(2, 'if x.startswith(("foo", "bar")):\n')]
+
     expected_diff = '--- \n+++ \n@@ -1,3 +1,3 @@\n x = \'foo\'\n-if x.startswith("foo") or x.startswith("bar"):\n+if x.startswith(("foo", "bar")):\n     print("Yes")\n'
     expected_line_change = "2"
     change_description = CombineStartswithEndswith.change_description
