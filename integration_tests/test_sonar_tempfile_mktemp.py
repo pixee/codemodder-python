9ce462519c8d256a7ed38754cc3c8a1a71642aec
clavedeluna <danalitovsky+git@gmail.com>
2024-04-01 09:28:57 -0300

    update integration tests

--- integration_tests/test_sonar_tempfile_mktemp.py
+++ integration_tests/test_sonar_tempfile_mktemp.py
@@ -5,8 +5,8 @@ from core_codemods.tempfile_mktemp import TempfileMktempTransformer
 
 class TestTempfileMktemp(SonarIntegrationTest):
     codemod = SonarTempfileMktemp
-    code_path = "tests/samples/tmpfile_mktemp.py"
+    code_path = "tests/samples/tempfile_mktemp.py"
     replacement_lines = [(3, "tempfile.mkstemp()\n")]
-    expected_diff = "--- \n+++ \n@@ -1,4 +1,4 @@\n import tempfile\n \n-tempfile.mktemp()\n+tempfile.mkstemp()\n"
+    expected_diff = "--- \n+++ \n@@ -1,3 +1,3 @@\n import tempfile\n \n-tempfile.mktemp()\n+tempfile.mkstemp()\n"
     expected_line_change = "3"
     change_description = TempfileMktempTransformer.change_description
