2b5d1042108b9adda7e45d05896637ba541f6450
andrecs <12188364+andrecsilva@users.noreply.github.com>
2024-04-22 08:42:54 -0300

    delete

---
+++ src/codemodder/sql_examples.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+
+def f(user):
+    sql = """SELECT user FROM users WHERE user = \'%s\'"""
+
+    conn = sqlite3.connect("example")
+    conn.cursor().execute(sql % (user))  # Noncompliant
+
+
+def g(user):
+    sql = "SELECT user FROM users WHERE user = '" + user + "'"
+
+    conn = sqlite3.connect("example")
+    conn.cursor().execute(sql)  # Noncompliant
