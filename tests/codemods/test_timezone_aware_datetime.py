6c6e186e3a34062ae93ada0b5ef344aebf81f51e
clavedeluna <danalitovsky+git@gmail.com>
2024-05-02 16:29:49 -0300

    working import alias tests except for `date`

--- tests/codemods/test_timezone_aware_datetime.py
+++ tests/codemods/test_timezone_aware_datetime.py
@@ -24,7 +24,7 @@ class TestTimezoneAwareDatetimeNeedKwarg(BaseCodemodTest):
         """
         self.run_and_assert(tmpdir, input_code, input_code)
 
-    def test_import_datetime(self, tmpdir):
+    def test_import(self, tmpdir):
         input_code = """
         import datetime
         import time
@@ -45,7 +45,28 @@ class TestTimezoneAwareDatetimeNeedKwarg(BaseCodemodTest):
         """
         self.run_and_assert(tmpdir, input_code, expected, num_changes=3)
 
-    def test_import_from_datetime(self, tmpdir):
+    def test_import_alias(self, tmpdir):
+        input_code = """
+        import datetime as mydate
+        import time
+        mydate.datetime(2021, 12, 25, 15, 30, 0)
+
+        mydate.datetime.now()
+
+        mydate.datetime.fromtimestamp(time.time())
+        """
+        expected = """
+        import datetime as mydate
+        import time
+        mydate.datetime(2021, 12, 25, 15, 30, 0, tzinfo=mydate.timezone.utc)
+
+        mydate.datetime.now(tz=mydate.timezone.utc)
+
+        mydate.datetime.fromtimestamp(time.time(), tz=mydate.timezone.utc)
+        """
+        self.run_and_assert(tmpdir, input_code, expected, num_changes=3)
+
+    def test_import_from(self, tmpdir):
         input_code = """
         from datetime import datetime
         import time
@@ -66,6 +87,27 @@ class TestTimezoneAwareDatetimeNeedKwarg(BaseCodemodTest):
         """
         self.run_and_assert(tmpdir, input_code, expected, num_changes=3)
 
+    def test_import_from_alias(self, tmpdir):
+        input_code = """
+        from datetime import datetime as mydate
+        import time
+        mydate(2021, 12, 25, 15, 30, 0)
+
+        mydate.now()
+
+        mydate.fromtimestamp(time.time())
+        """
+        expected = """
+        from datetime import timezone, datetime as mydate
+        import time
+        mydate(2021, 12, 25, 15, 30, 0, tzinfo=timezone.utc)
+
+        mydate.now(tz=timezone.utc)
+
+        mydate.fromtimestamp(time.time(), tz=timezone.utc)
+        """
+        self.run_and_assert(tmpdir, input_code, expected, num_changes=3)
+
 
 class TestTimezoneAwareDatetimeReplaceFunc(BaseCodemodTest):
     codemod = TimezoneAwareDatetime
@@ -78,7 +120,7 @@ class TestTimezoneAwareDatetimeReplaceFunc(BaseCodemodTest):
         """
         self.run_and_assert(tmpdir, input_code, input_code)
 
-    def test_import_datetime(self, tmpdir):
+    def test_import(self, tmpdir):
         input_code = """
         import datetime
         import time
@@ -103,7 +145,7 @@ class TestTimezoneAwareDatetimeReplaceFunc(BaseCodemodTest):
         from zoneinfo import ZoneInfo
         
         datetime.datetime.now(tz=datetime.timezone.utc)
-        datetime.datetime.now(tz=datetime.timezone.utc)
+        datetime.datetime.now(tz=datetime.timezone.utc).date()
         datetime.datetime.now(tz=datetime.timezone.utc)
         
         datetime.datetime.fromtimestamp(time.time(), tz=datetime.timezone.utc).date()
@@ -117,7 +159,46 @@ class TestTimezoneAwareDatetimeReplaceFunc(BaseCodemodTest):
         """
         self.run_and_assert(tmpdir, input_code, expected, num_changes=9)
 
-    def test_import_from_datetime(self, tmpdir):
+    def test_import_alias(self, tmpdir):
+        input_code = """
+        import datetime as mydate
+        import time
+        from zoneinfo import ZoneInfo
+
+        mydate.datetime.today()
+        mydate.date.today()
+        mydate.datetime.utcnow()
+
+        mydate.date.fromtimestamp(time.time())
+        eastern =  ZoneInfo("America/New_York")
+        mydate.date.fromtimestamp(time.time(), eastern)
+        mydate.date.fromtimestamp(time.time(), tz=eastern)
+
+        mydate.datetime.utcfromtimestamp(time.time())
+        mydate.datetime.utcfromtimestamp(time.time(), eastern)
+        mydate.datetime.utcfromtimestamp(time.time(), tz=eastern)
+        """
+        expected = """
+        import datetime as mydate
+        import time
+        from zoneinfo import ZoneInfo
+
+        mydate.datetime.now(tz=mydate.timezone.utc)
+        mydate.datetime.now(tz=mydate.timezone.utc).date()
+        mydate.datetime.now(tz=mydate.timezone.utc)
+
+        mydate.datetime.fromtimestamp(time.time(), tz=mydate.timezone.utc).date()
+        eastern =  ZoneInfo("America/New_York")
+        mydate.datetime.fromtimestamp(time.time(), eastern).date()
+        mydate.datetime.fromtimestamp(time.time(), tz=eastern).date()
+
+        mydate.datetime.fromtimestamp(time.time(), tz=mydate.timezone.utc)
+        mydate.datetime.fromtimestamp(time.time(), eastern)
+        mydate.datetime.fromtimestamp(time.time(), tz=eastern)
+        """
+        self.run_and_assert(tmpdir, input_code, expected, num_changes=9)
+
+    def test_import_from(self, tmpdir):
         input_code = """
         from datetime import date, datetime
         import time
@@ -142,7 +223,7 @@ class TestTimezoneAwareDatetimeReplaceFunc(BaseCodemodTest):
         from zoneinfo import ZoneInfo
 
         datetime.now(tz=timezone.utc)
-        datetime.now(tz=timezone.utc)
+        datetime.now(tz=timezone.utc).date()
         datetime.now(tz=timezone.utc)
 
         datetime.fromtimestamp(time.time(), tz=timezone.utc).date()
@@ -156,4 +237,48 @@ class TestTimezoneAwareDatetimeReplaceFunc(BaseCodemodTest):
         """
         self.run_and_assert(tmpdir, input_code, expected, num_changes=9)
 
-        # date should be removed from import, but test that it is kept if it is used
+    def test_import_from_alias(self, tmpdir):
+        input_code = """
+        from datetime import date, datetime as mydate
+        import time
+        from zoneinfo import ZoneInfo
+
+        mydate.today()
+        date.today()
+        mydate.utcnow()
+
+        date.fromtimestamp(time.time())
+        eastern =  ZoneInfo("America/New_York")
+        date.fromtimestamp(time.time(), eastern)
+        date.fromtimestamp(time.time(), tz=eastern)
+
+        mydate.utcfromtimestamp(time.time())
+        mydate.utcfromtimestamp(time.time(), eastern)
+        mydate.utcfromtimestamp(time.time(), tz=eastern)
+        """
+        expected = """
+        from datetime import timezone, date, datetime as mydate
+        import time
+        from zoneinfo import ZoneInfo
+
+        mydate.now(tz=timezone.utc)
+        mydate.now(tz=timezone.utc).date()
+        mydate.now(tz=timezone.utc)
+
+        mydate.fromtimestamp(time.time(), tz=timezone.utc).date()
+        eastern =  ZoneInfo("America/New_York")
+        mydate.fromtimestamp(time.time(), eastern).date()
+        mydate.fromtimestamp(time.time(), tz=eastern).date()
+
+        mydate.fromtimestamp(time.time(), tz=timezone.utc)
+        mydate.fromtimestamp(time.time(), eastern)
+        mydate.fromtimestamp(time.time(), tz=eastern)
+        """
+        self.run_and_assert(tmpdir, input_code, expected, num_changes=9)
+
+    # def test_import_date(self, tmpdir):
+    #     input_code = """
+    #     from datetime import date
+    #     date.today()
+    #     """
+    #     # need to handle original node being "date", import from, alias, etc
