cd81a0d2d27c073d674f4d895d961d0e3b5a99d8
clavedeluna <danalitovsky+git@gmail.com>
2024-05-22 08:59:02 -0300

    tmpfile codemod able to report unfixed

--- src/core_codemods/tempfile_mktemp.py
+++ src/core_codemods/tempfile_mktemp.py
@@ -3,12 +3,16 @@ from typing import Optional
 
 import libcst as cst
 from libcst import matchers
+from libcst.codemod import CodemodContext
+from libcst.metadata import ScopeProvider
 
 from codemodder.codemods.libcst_transformer import (
     LibcstResultTransformer,
     LibcstTransformerPipeline,
 )
 from codemodder.codemods.utils_mixin import NameAndAncestorResolutionMixin
+from codemodder.file_context import FileContext
+from codemodder.result import Result, same_line
 from codemodder.utils.utils import clean_simplestring
 from core_codemods.api import CoreCodemod, Metadata, Reference, ReviewGuidance
 
@@ -19,7 +23,21 @@ class TempfileMktempTransformer(
     change_description = "Replaces `tempfile.mktemp` with `tempfile.mkstemp`."
     _module_name = "tempfile"
 
+    def __init__(
+        self,
+        context: CodemodContext,
+        results: list[Result] | None,
+        file_context: FileContext,
+        _transformer: bool = False,
+    ):
+        self.mktemp_calls: set[cst.Call] = set()
+        super().__init__(context, results, file_context, _transformer)
+
+    def visit_Call(self, node: cst.Call) -> None:
+        self.mktemp_calls.add(node) if self._is_mktemp_call(node) else None
+
     def leave_SimpleStatementLine(self, original_node, updated_node):
+        # todo collapse this method into one below
         match original_node:
             case cst.SimpleStatementLine(body=[bsstmt]):
                 return self.check_mktemp(original_node, bsstmt)
@@ -28,13 +46,39 @@ class TempfileMktempTransformer(
     def check_mktemp(
         self, original_node: cst.SimpleStatementLine, bsstmt: cst.BaseSmallStatement
     ) -> cst.SimpleStatementLine | cst.FlattenSentinel:
+        # add include/exclude test
+
+        if not self.node_is_selected(original_node):
+            return original_node
+
         if maybe_tuple := self._is_assigned_to_mktemp(bsstmt):  # type: ignore
             assign_name, call = maybe_tuple
             return self.report_and_change(call, assign_name)
         if maybe_tuple := self._mktemp_is_sink(bsstmt):
             wrapper_func_name, call = maybe_tuple
             return self.report_and_change(call, wrapper_func_name, assignment=False)
+
+        # If we get here it's because there is a mktemp call but we haven't fixed it yet.
+        for unfixed in self.mktemp_calls:
+            self.report_unfixed(unfixed, reason="Pixee does not yet support this fix.")
+        self.mktemp_calls.clear()
         return original_node
+
+    def filter_by_result(self, node) -> bool:
+        """
+        TODO
+        """
+        match node:
+            case cst.SimpleStatementLine():
+                pos_to_match = self.node_position(node)
+                return self.results is None or any(
+                    self.match_location(pos_to_match, result)
+                    for result in self.results or []
+                )
+        return False
+
+    def match_location(self, pos, result):
+        return any(same_line(pos, location) for location in result.locations)
 
     def report_and_change(
         self, node: cst.Call, name: cst.Name, assignment=True
@@ -113,6 +157,21 @@ class TempfileMktempTransformer(
                             wrapper_func = call.func
                             return (wrapper_func, maybe_value)
         return None
+
+
+class GatherMkTempCallsVisitor(cst.CSTVisitor, NameAndAncestorResolutionMixin):
+    METADATA_DEPENDENCIES = (ScopeProvider,)
+
+    def __init__(self):
+        super().__init__()
+        self.calls = []
+
+    def visit_Call(self, node: cst.Call) -> None:
+        (
+            self.calls.append(node)
+            if self.find_base_name(node.func) == "tempfile.mktemp"
+            else None
+        )
 
 
 TempfileMktemp = CoreCodemod(
