da219be3f1931896da0646b7957e4db4b142303c
clavedeluna <danalitovsky+git@gmail.com>
2024-07-09 10:04:13 -0300

    fix import

--- integration_tests/sonar/test_sonar_jwt_decode_verify.py
+++ integration_tests/sonar/test_sonar_jwt_decode_verify.py
@@ -1,6 +1,6 @@
 from codemodder.codemods.test import SonarIntegrationTest
 from core_codemods.sonar.sonar_jwt_decode_verify import (
-    JwtDecodeVerifySonarTransformer,
+    JwtDecodeVerifySASTTransformer,
     SonarJwtDecodeVerify,
 )
 
@@ -22,4 +22,4 @@ class TestJwtDecodeVerify(SonarIntegrationTest):
     expected_diff = '--- \n+++ \n@@ -8,7 +8,7 @@\n \n encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm="HS256")\n \n-decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], verify=False)\n-decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], options={"verify_signature": False})\n+decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], verify=True)\n+decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], options={"verify_signature": True})\n \n var = "something"\n'
     expected_line_change = "11"
     num_changes = 2
-    change_description = JwtDecodeVerifySonarTransformer.change_description
+    change_description = JwtDecodeVerifySASTTransformer.change_description
