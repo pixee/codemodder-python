aae02a62789c3c7941ccad96fbf3be750ad75439
clavedeluna <danalitovsky+git@gmail.com>
2024-07-10 11:51:23 -0300

    semgrep from core codemod to allow list of rules

--- src/core_codemods/semgrep/semgrep_enable_jinja2_autoescape.py
+++ src/core_codemods/semgrep/semgrep_enable_jinja2_autoescape.py
@@ -1,9 +1,17 @@
+from codemodder.codemods.base_codemod import ToolRule
 from core_codemods.enable_jinja2_autoescape import EnableJinja2Autoescape
-from core_codemods.semgrep.api import SemgrepCodemod
+from core_codemods.semgrep.api import SemgrepCodemod, semgrep_url_from_id
 
 SemgrepEnableJinja2Autoescape = SemgrepCodemod.from_core_codemod(
     name="enable-jinja2-autoescape",
     other=EnableJinja2Autoescape,
-    rule_id="python.flask.security.xss.audit.direct-use-of-jinja2.direct-use-of-jinja2",
-    rule_name="direct-use-of-jinja2",
+    rules=[
+        ToolRule(
+            id=(
+                rule_id := "python.flask.security.xss.audit.direct-use-of-jinja2.direct-use-of-jinja2"
+            ),
+            name="direct-use-of-jinja2",
+            url=semgrep_url_from_id(rule_id),
+        )
+    ],
 )
