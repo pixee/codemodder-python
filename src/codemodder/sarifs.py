c6a91dd5edb700b2841b0d952ccba4813a042bba
Dan D'Avella <dan.davella@pixee.ai>
2024-07-25 09:12:32 -0400

    Prevent multiple SARIF inputs from the same tool (#742)

--- src/codemodder/sarifs.py
+++ src/codemodder/sarifs.py
@@ -15,6 +15,9 @@ class AbstractSarifToolDetector(metaclass=ABCMeta):
         pass
 
 
+class DuplicateToolError(ValueError): ...
+
+
 def detect_sarif_tools(filenames: list[Path]) -> DefaultDict[str, list[str]]:
     results: DefaultDict[str, list[str]] = defaultdict(list)
 
@@ -30,7 +33,15 @@ def detect_sarif_tools(filenames: list[Path]) -> DefaultDict[str, list[str]]:
                 try:
                     if det.detect(run):
                         logger.debug("detected %s sarif: %s", name, fname)
+                        # According to the Codemodder spec, it is invalid to have multiple SARIF results for the same tool
+                        # https://github.com/pixee/codemodder-specs/pull/36
+                        if name in results:
+                            raise DuplicateToolError(
+                                f"duplicate tool sarif detected: {name}"
+                            )
                         results[name].append(str(fname))
+                except DuplicateToolError as err:
+                    raise err
                 except (KeyError, AttributeError, ValueError):
                     continue
 
