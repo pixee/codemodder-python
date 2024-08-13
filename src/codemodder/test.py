3f505f24d8a36303bdeb1f516a85670b89fc171a
clavedeluna <danalitovsky+git@gmail.com>
2024-08-13 10:58:05 -0300

    test sonar

---
+++ src/codemodder/test.py
@@ -0,0 +1,30 @@
+a = 5
+i = 20
+x = a
+y = x
+z = x
+n = 5
+m = 0
+status = True
+
+
+def finished():
+    return True
+
+
+user_input = "hi"
+
+if not a == 2:
+    b = not i < 10
+
+if not x != y:
+    z = not m <= n
+
+if not status is True:
+    pass
+
+if not finished() is False:
+    pass
+
+if not user_input == "yes":
+    pass
