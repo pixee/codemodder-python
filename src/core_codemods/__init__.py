f6c0ccbb9d3049919bb3a426c50e0a875ddf8094
clavedeluna <danalitovsky+git@gmail.com>
2024-08-19 09:37:27 -0300

    add core codemod

--- src/core_codemods/__init__.py
+++ src/core_codemods/__init__.py
@@ -95,6 +95,7 @@ from .sql_parameterization import SQLQueryParameterization
 from .str_concat_in_seq_literal import StrConcatInSeqLiteral
 from .subprocess_shell_false import SubprocessShellFalse
 from .tempfile_mktemp import TempfileMktemp
+from .timezone_aware_datetime import TimezoneAwareDatetime
 from .upgrade_sslcontext_minimum_version import UpgradeSSLContextMinimumVersion
 from .upgrade_sslcontext_tls import UpgradeSSLContextTLS
 from .url_sandbox import UrlSandbox
@@ -136,6 +137,7 @@ registry = CodemodCollection(
         UseDefusedXml,
         UseGenerator,
         UseSetLiteral,
+        TimezoneAwareDatetime,
         UseWalrusIf,
         WithThreadingLock,
         SQLQueryParameterization,
