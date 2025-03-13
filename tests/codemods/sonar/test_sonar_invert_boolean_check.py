c66929d45351e65166b1fa68f1c20488e65e0fb3
andrecs <12188364+andrecsilva@users.noreply.github.com>
2025-03-13 10:03:12 -0300

    Fixed a few more unit tests

--- tests/codemods/sonar/test_sonar_invert_boolean_check.py
+++ tests/codemods/sonar/test_sonar_invert_boolean_check.py
@@ -20,6 +20,27 @@ class TestSonarInvertedBooleanCheck(BaseSASTCodemodTest):
         if a != 2:
             b = i >= 10
         """
+        expected_diff_per_change = [
+            """\
+--- 
++++ 
+@@ -1,3 +1,3 @@
+ 
+-if not a == 2:
++if a != 2:
+     b = not i < 10
+""",
+            """\
+--- 
++++ 
+@@ -1,3 +1,3 @@
+ 
+ if not a == 2:
+-    b = not i < 10
++    b = i >= 10
+""",
+        ]
+
         issues = {
             "issues": [
                 {
@@ -50,6 +71,7 @@ class TestSonarInvertedBooleanCheck(BaseSASTCodemodTest):
             tmpdir,
             input_code,
             expected_output,
+            expected_diff_per_change,
             results=json.dumps(issues),
             num_changes=2,
         )
