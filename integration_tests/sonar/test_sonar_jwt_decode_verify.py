79ba294c84b7a7e5eb01689d9d29cfdd74dfc3a8
andrecs <12188364+andrecsilva@users.noreply.github.com>
2025-03-20 09:16:03 -0300

    Added integration tests for new behavior

--- integration_tests/sonar/test_sonar_jwt_decode_verify.py
+++ integration_tests/sonar/test_sonar_jwt_decode_verify.py
@@ -1,25 +1,19 @@
-from codemodder.codemods.test import SonarIntegrationTest
+from codemodder.codemods.test.integration_utils import SonarRemediationIntegrationTest
 from core_codemods.sonar.sonar_jwt_decode_verify import (
     JwtDecodeVerifySASTTransformer,
     SonarJwtDecodeVerify,
 )
 
 
-class TestJwtDecodeVerify(SonarIntegrationTest):
+class TestJwtDecodeVerify(SonarRemediationIntegrationTest):
     codemod = SonarJwtDecodeVerify
     code_path = "tests/samples/jwt_decode_verify.py"
-    replacement_lines = [
-        (
-            11,
-            """decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], verify=True)\n""",
-        ),
-        (
-            12,
-            """decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], options={"verify_signature": True})\n""",
-        ),
+
+    expected_diff_per_change = [
+        '--- \n+++ \n@@ -8,7 +8,7 @@\n \n encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm="HS256")\n \n-decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], verify=False)\n+decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], verify=True)\n decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], options={"verify_signature": False})\n \n var = "something"\n',
+        '--- \n+++ \n@@ -9,6 +9,6 @@\n encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm="HS256")\n \n decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], verify=False)\n-decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], options={"verify_signature": False})\n+decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], options={"verify_signature": True})\n \n var = "something"\n',
     ]
 
-    expected_diff = '--- \n+++ \n@@ -8,7 +8,7 @@\n \n encoded_jwt = jwt.encode(payload, SECRET_KEY, algorithm="HS256")\n \n-decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], verify=False)\n-decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], options={"verify_signature": False})\n+decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], verify=True)\n+decoded_payload = jwt.decode(encoded_jwt, SECRET_KEY, algorithms=["HS256"], options={"verify_signature": True})\n \n var = "something"\n'
-    expected_line_change = "11"
+    expected_lines_changed = [11, 12]
     num_changes = 2
     change_description = JwtDecodeVerifySASTTransformer.change_description
