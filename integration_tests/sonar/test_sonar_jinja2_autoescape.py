635d74cffe01a3ab6eab4269e90d78f5779ce0ca
andrecs <12188364+andrecsilva@users.noreply.github.com>
2025-03-21 09:11:39 -0300

    Fixed some integration tests

--- integration_tests/sonar/test_sonar_jinja2_autoescape.py
+++ integration_tests/sonar/test_sonar_jinja2_autoescape.py
@@ -1,18 +1,18 @@
-from codemodder.codemods.test import SonarIntegrationTest
+from codemodder.codemods.test.integration_utils import SonarRemediationIntegrationTest
 from core_codemods.enable_jinja2_autoescape import EnableJinja2AutoescapeTransformer
 from core_codemods.sonar.sonar_enable_jinja2_autoescape import (
     SonarEnableJinja2Autoescape,
 )
 
 
-class TestSonarEnableJinja2Autoescape(SonarIntegrationTest):
+class TestSonarEnableJinja2Autoescape(SonarRemediationIntegrationTest):
     codemod = SonarEnableJinja2Autoescape
     code_path = "tests/samples/jinja2_autoescape.py"
-    replacement_lines = [
-        (3, "env = Environment(autoescape=True)\n"),
-        (4, "env = Environment(autoescape=True)\n"),
+    expected_diff_per_change = [
+        "--- \n+++ \n@@ -1,4 +1,4 @@\n from jinja2 import Environment\n \n-env = Environment()\n+env = Environment(autoescape=True)\n env = Environment(autoescape=False)\n",
+        "--- \n+++ \n@@ -1,4 +1,4 @@\n from jinja2 import Environment\n \n env = Environment()\n-env = Environment(autoescape=False)\n+env = Environment(autoescape=True)\n",
     ]
-    expected_diff = "--- \n+++ \n@@ -1,4 +1,4 @@\n from jinja2 import Environment\n \n-env = Environment()\n-env = Environment(autoescape=False)\n+env = Environment(autoescape=True)\n+env = Environment(autoescape=True)\n"
-    expected_line_change = "3"
+
+    expected_lines_changed = [3, 4]
     num_changes = 2
     change_description = EnableJinja2AutoescapeTransformer.change_description
