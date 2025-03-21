0d7d041e92101187e1d19939b27930ef0b517c8b
andrecs <12188364+andrecsilva@users.noreply.github.com>
2025-03-21 09:11:39 -0300

    Fixed some integration tests

--- integration_tests/test_request_verify.py
+++ integration_tests/test_request_verify.py
@@ -1,10 +1,10 @@
 from requests import exceptions
 
-from codemodder.codemods.test import BaseIntegrationTest
+from codemodder.codemods.test.integration_utils import BaseRemediationIntegrationTest
 from core_codemods.requests_verify import RequestsVerify
 
 
-class TestRequestsVerify(BaseIntegrationTest):
+class TestRequestsVerify(BaseRemediationIntegrationTest):
     codemod = RequestsVerify
     original_code = """
     import requests
@@ -14,15 +14,12 @@ class TestRequestsVerify(BaseIntegrationTest):
     var = "hello"
     """
 
-    replacement_lines = [
-        (3, """requests.get("https://www.google.com", verify=True)\n"""),
-        (
-            4,
-            """requests.post("https://some-api/", json={"id": 1234, "price": 18}, verify=True)\n""",
-        ),
+    expected_diff_per_change = [
+        '--- \n+++ \n@@ -1,5 +1,5 @@\n import requests\n \n-requests.get("https://www.google.com", verify=False)\n+requests.get("https://www.google.com", verify=True)\n requests.post("https://some-api/", json={"id": 1234, "price": 18}, verify=False)\n var = "hello"',
+        '--- \n+++ \n@@ -1,5 +1,5 @@\n import requests\n \n requests.get("https://www.google.com", verify=False)\n-requests.post("https://some-api/", json={"id": 1234, "price": 18}, verify=False)\n+requests.post("https://some-api/", json={"id": 1234, "price": 18}, verify=True)\n var = "hello"',
     ]
-    expected_diff = '--- \n+++ \n@@ -1,5 +1,5 @@\n import requests\n \n-requests.get("https://www.google.com", verify=False)\n-requests.post("https://some-api/", json={"id": 1234, "price": 18}, verify=False)\n+requests.get("https://www.google.com", verify=True)\n+requests.post("https://some-api/", json={"id": 1234, "price": 18}, verify=True)\n var = "hello"\n'
-    expected_line_change = "3"
+
+    expected_lines_changed = [3, 4]
     num_changes = 2
     change_description = RequestsVerify.change_description
     # expected because when executing the output code it will make a request which fails, which is OK.
