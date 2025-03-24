43b23153a3a5c22a17cb451250d76d8c60b98a84
andrecs <12188364+andrecsilva@users.noreply.github.com>
2025-03-24 08:01:21 -0300

    More integration tests converted

--- integration_tests/test_jinja2_autoescape.py
+++ integration_tests/test_jinja2_autoescape.py
@@ -1,11 +1,11 @@
-from codemodder.codemods.test import BaseIntegrationTest
+from codemodder.codemods.test.integration_utils import BaseRemediationIntegrationTest
 from core_codemods.enable_jinja2_autoescape import (
     EnableJinja2Autoescape,
     EnableJinja2AutoescapeTransformer,
 )
 
 
-class TestEnableJinja2Autoescape(BaseIntegrationTest):
+class TestEnableJinja2Autoescape(BaseRemediationIntegrationTest):
     codemod = EnableJinja2Autoescape
     original_code = """
     from jinja2 import Environment
@@ -13,11 +13,12 @@ class TestEnableJinja2Autoescape(BaseIntegrationTest):
     env = Environment()
     env = Environment(autoescape=False)
     """
-    replacement_lines = [
-        (3, "env = Environment(autoescape=True)\n"),
-        (4, "env = Environment(autoescape=True)\n"),
+
+    expected_diff_per_change = [
+        "--- \n+++ \n@@ -1,4 +1,4 @@\n from jinja2 import Environment\n \n-env = Environment()\n+env = Environment(autoescape=True)\n env = Environment(autoescape=False)",
+        "--- \n+++ \n@@ -1,4 +1,4 @@\n from jinja2 import Environment\n \n env = Environment()\n-env = Environment(autoescape=False)\n+env = Environment(autoescape=True)",
     ]
-    expected_diff = "--- \n+++ \n@@ -1,4 +1,4 @@\n from jinja2 import Environment\n \n-env = Environment()\n-env = Environment(autoescape=False)\n+env = Environment(autoescape=True)\n+env = Environment(autoescape=True)\n"
-    expected_line_change = "3"
+
+    expected_lines_changed = [3, 4]
     num_changes = 2
     change_description = EnableJinja2AutoescapeTransformer.change_description
