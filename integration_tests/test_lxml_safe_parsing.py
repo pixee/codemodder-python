3df2907038ebeb4f54d168f5291149314f24ce19
andrecs <12188364+andrecsilva@users.noreply.github.com>
2025-03-24 08:01:21 -0300

    More integration tests converted

--- integration_tests/test_lxml_safe_parsing.py
+++ integration_tests/test_lxml_safe_parsing.py
@@ -1,26 +1,19 @@
-from codemodder.codemods.test import BaseIntegrationTest
+from codemodder.codemods.test.integration_utils import BaseRemediationIntegrationTest
 from core_codemods.lxml_safe_parsing import LxmlSafeParsing
 
 
-class TestLxmlSafeParsing(BaseIntegrationTest):
+class TestLxmlSafeParsing(BaseRemediationIntegrationTest):
     codemod = LxmlSafeParsing
     original_code = """
     import lxml.etree
     lxml.etree.parse("path_to_file")
     lxml.etree.fromstring("xml_str")
     """
-    replacement_lines = [
-        (
-            2,
-            'lxml.etree.parse("path_to_file", parser=lxml.etree.XMLParser(resolve_entities=False))\n',
-        ),
-        (
-            3,
-            'lxml.etree.fromstring("xml_str", parser=lxml.etree.XMLParser(resolve_entities=False))\n',
-        ),
+    expected_lines_changed = [2, 3]
+    expected_diff_per_change = [
+        '--- \n+++ \n@@ -1,3 +1,3 @@\n import lxml.etree\n-lxml.etree.parse("path_to_file")\n+lxml.etree.parse("path_to_file", parser=lxml.etree.XMLParser(resolve_entities=False))\n lxml.etree.fromstring("xml_str")',
+        '--- \n+++ \n@@ -1,3 +1,3 @@\n import lxml.etree\n lxml.etree.parse("path_to_file")\n-lxml.etree.fromstring("xml_str")\n+lxml.etree.fromstring("xml_str", parser=lxml.etree.XMLParser(resolve_entities=False))',
     ]
-    expected_diff = '--- \n+++ \n@@ -1,3 +1,3 @@\n import lxml.etree\n-lxml.etree.parse("path_to_file")\n-lxml.etree.fromstring("xml_str")\n+lxml.etree.parse("path_to_file", parser=lxml.etree.XMLParser(resolve_entities=False))\n+lxml.etree.fromstring("xml_str", parser=lxml.etree.XMLParser(resolve_entities=False))\n'
-    expected_line_change = "2"
     num_changes = 2
     change_description = LxmlSafeParsing.change_description
     allowed_exceptions = (OSError,)
