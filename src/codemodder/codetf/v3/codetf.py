d6a192ea3f4b067ababab99c8c53f059f92a4655
André C. Silva <12188364+andrecsilva@users.noreply.github.com>
2025-05-14 07:26:04 -0300

    Added transform for CodeTFv2 models to CodeTFv3 (#1061)
    * Added transform for CodeTFv2 models to CodeTFv3
    
    * Refactored test

--- src/codemodder/codetf/v3/codetf.py
+++ src/codemodder/codetf/v3/codetf.py
@@ -6,7 +6,11 @@ from typing import Optional
 from pydantic import BaseModel, model_validator
 
 from ..common import Change, CodeTFWriter, Finding, FixQuality
+from ..v2.codetf import AIMetadata as AIMetadatav2
+from ..v2.codetf import CodeTF as CodeTFv2
 from ..v2.codetf import Finding as V2Finding
+from ..v2.codetf import Result
+from ..v2.codetf import Run as Runv2
 
 
 class Run(BaseModel):
@@ -15,17 +19,17 @@ class Run(BaseModel):
     vendor: str
     tool: str
     version: str
-    # Optional free-form metadata about the project being analyzed
-    # e.g. project name, directory, commit SHA, etc.
-    projectMetadata: Optional[str] = None
-    # Analysis duration in milliseconds
-    elapsed: Optional[int] = None
-    # Optional free-form metadata about the inputs used for the analysis
+    # optional free-form metadata about the project being analyzed
+    # e.g. project name, directory, commit sha, etc.
+    projectmetadata: dict | None = None
+    # analysis duration in milliseconds
+    elapsed: int | None = None
+    # optional free-form metadata about the inputs used for the analysis
     # e.g. command line, environment variables, etc.
-    inputMetadata: Optional[dict] = None
-    # Optional free-form metadata about the analysis itself
+    inputmetadata: dict | None = None
+    # optional free-form metadata about the analysis itself
     # e.g. timeouts, memory usage, etc.
-    analysisMetadata: Optional[dict] = None
+    analysismetadata: dict | None = None
 
 
 class FixStatusType(str, Enum):
@@ -116,3 +120,81 @@ class FixResult(BaseModel):
 class CodeTF(CodeTFWriter, BaseModel):
     run: Run
     results: list[FixResult]
+
+
+def from_v2_run(run: Runv2) -> Run:
+    project_metadata = {"directory": run.directory} | (
+        {"projectName": run.projectName} if run.projectName else {}
+    )
+    input_metadata = {"commandLine": run.commandLine} | (
+        {"sarifs": run.sarifs} if run.sarifs else {}
+    )
+
+    return Run(
+        vendor=run.vendor,
+        tool=run.tool,
+        version=run.version,
+        elapsed=run.elapsed,
+        projectmetadata=project_metadata,
+        inputmetadata=input_metadata,
+    )
+
+
+def from_v2_aimetadata(ai_metadata: AIMetadatav2) -> AIMetadata:
+    return AIMetadata(
+        provider=ai_metadata.provider,
+        models=[ai_metadata.model] if ai_metadata.model else None,
+        total_tokens=ai_metadata.tokens,
+        completion_tokens=ai_metadata.completion_tokens,
+    )
+
+
+def from_v2_result(result: Result) -> list[FixResult]:
+    fix_results: list[FixResult] = []
+    # generate fixed
+    for cs in result.changeset:
+        # No way of identifying hybrid AI codemods by the metadata alone
+        generation_metadata = GenerationMetadata(
+            strategy=Strategy.ai if cs.ai else Strategy.deterministic,
+            ai=from_v2_aimetadata(cs.ai) if cs.ai else None,
+            provisional=False,
+        )
+        for c in cs.changes:
+            for f in c.fixedFindings or []:
+                fix_metadata = FixMetadata(
+                    id=result.codemod,
+                    summary=result.summary,
+                    description=result.description,
+                    generation=generation_metadata,
+                )
+                # Retrieve diff from changeset since individual diffs per change may not exist
+                # If the codetf was generated with per-finding, each ChangeSet will have a single change anyway
+                changeset = ChangeSet(
+                    path=cs.path, diff=cs.diff, changes=[c.to_common()]
+                )
+                fix_results.append(
+                    FixResult(
+                        finding=f,
+                        fixStatus=FixStatus(status=FixStatusType.fixed),
+                        changeSets=[changeset],
+                        fixMetadata=fix_metadata,
+                    )
+                )
+
+    # generate unfixed
+    for f in result.unfixedFindings or []:
+        fix_results.append(
+            FixResult(
+                finding=f,
+                fixStatus=FixStatus(status=FixStatusType.failed, reason=f.reason),
+            )
+        )
+
+    return fix_results
+
+
+def from_v2(codetf: CodeTFv2) -> CodeTF:
+    return CodeTF(
+        run=from_v2_run(codetf.run),
+        results=[fr for result in codetf.results for fr in from_v2_result(result)],
+    )
